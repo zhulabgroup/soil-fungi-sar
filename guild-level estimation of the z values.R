31de41f84039c13e601e71df7f81df335d455e25
wenqisysu <wenqil@umich.edu>
2023-10-22 17:12:37 -0400

    add

--- guild-level estimation of the z values.R
+++ guild-level estimation of the z values.R
@@ -148,7 +148,7 @@ d<- merge_phyloseq(m, plotIDM)# merge the new plotid with the initial data
 a1= sample_data(d)
 a1=unique(a1$plotIDM)
 
-set.seed(10102)
+set.seed(10104)
 times=30
 power.z <- vector("list", length(a1))
 
@@ -199,7 +199,7 @@ for(i in 1:length(a1)){
 plantpat_c=data.frame(a1,plantpat_c)
 write.csv(all_c,"plantpat_c.ranall.csv")
 
-3.# for the ECM
+3.# for the ECM guild
 
 m=subset_taxa(rare_all,ta2=="ectomycorrhizal" )# select a guild
 m<- subset_samples(m, !is.na(lon) & !is.na(lat))
@@ -434,7 +434,7 @@ litsap_c=data.frame(a1,litsap_c)
 write.csv(litsap_c,"litsap_c.ranall.csv")
 #
 
-5.# for the litter_saprotroph
+5.# for the litter_saprotroph guild
 m=subset_taxa(rare_all,ta2=="litter_saprotroph" )# select a guild
 m<- subset_samples(m, !is.na(lon) & !is.na(lat))
 m<- subset_taxa(m, taxa_sums(m) > 0)
@@ -510,5 +510,164 @@ for(i in 1:length(a1)){
 
 litsap_c=data.frame(a1,litsap_c)
 write.csv(litsap_c,"litsap_c.ranall.csv")
+#
+
+6.# for the wood_saprotroph
+m=subset_taxa(rare_all,ta2=="wood_saprotroph" )# select a guild
+m<- subset_samples(m, !is.na(lon) & !is.na(lat))
+m<- subset_taxa(m, taxa_sums(m) > 0)
+m<- subset_samples(m, sample_sums(m) > 0)
+
+
+# add a plotID for the data
+
+d=sample_data(m)
+table(d$Project)# the first 908 rows are dob sites while the remaining 5470 rows are NEON sites
+d1=data.frame(d[,c("geneticSampleID","Site")])
+plotID=substr(d1$geneticSampleID,1,8)
+d1=cbind(d1,plotID)
+iddob=d1$Site[1:table(d$Project)[[1]]]#a site corresponds to a plot
+idneon=d1$plotID[(table(d$Project)[[1]]+1):sum(table(d$Project))]# a plotID corresponds to a plot
+plotIDM=data.frame(c(iddob,idneon))
+
+names(plotIDM)="plotIDM"# the plotID used for the SAR
+row.names(plotIDM)=row.names(sample_data(m))#an important step to merge the data
+plotIDM=sample_data(plotIDM)
+d<- merge_phyloseq(m, plotIDM)# merge the new plotid with the initial data 
+
+a1= sample_data(d)
+a1=unique(a1$plotIDM)
+
+set.seed(10102)
+times=30
+power.z <- vector("list", length(a1))
+
+for (i in 1:length(a1))
+{
+  cat('\r',paste(paste0(rep("*", round(i/ 1, 0)), collapse = ''), i, collapse = ''))# informs the processing
+  data_sub <- subset_samples(d, plotIDM==a1[i])
+  dim1 <- dim(otu_table(data_sub)) # the number of samples in one site
+  if (dim1[1] >= 3)# we can construct a linear regression model with at least three cores
+  {
+    cl <- makeCluster(3)
+    registerDoParallel(cl)
+    power.z[[i]] <- foreach(i = 1:times, .combine = "cbind", .packages = c("phyloseq")) %dopar%{
+      species <- vector(length = dim1[1]) # create a vector to save diversity
+      for (j in 1:dim1[1]) 
+      { 
+        # randomly sample j samples in the plot
+        flag <- rep(FALSE, dim1[1])
+        flag[sample(1:dim1[1], j)] <- TRUE
+        temp <- merge_samples(data_sub, flag, function(x) mean(x, na.rm = TRUE)) # the j samples aggregated by mean
+        species[j] <- sum(otu_table(temp)["TRUE"] > 0)
+      }
+      ex <- as.data.frame(cbind("A"=c(1:dim1[1]),species))
+      temp<- summary(lm(log(species)~log(A),ex))[["coefficients"]]# extract the estimated c and z values
+      return(c(temp[2,1], temp[1,1]))
+    }
+    stopCluster(cl)
+  }
+  else
+  {
+    power.z[[i]]=matrix(10,nrow=2,ncol = dim1[1])#the number 10 is randomly selected to create a matrix for the plots with < 3 cores to avoid NULL output
+  }
+}
+
+woosap_z=matrix(nrow=length(a1),ncol=30)# get the 30 simulated z values for all the plots
+for(i in 1:length(a1)){
+  woosap_z[i,]=power.z[[i]][1,]
+}
+
+woosap_z=data.frame(a1,woosap_z)
+write.csv(woosap_z,"woosap_z.ranall.csv")
+
+woosap_c=matrix(nrow=length(a1),ncol=30)# get the 30 simulated c values for the neon site
+for(i in 1:length(a1)){
+  woosap_c[i,]=power.z[[i]][2,]
+}
+
+woosap_c=data.frame(a1,woosap_c)
+write.csv(woosap_c,"woosap_c.ranall.csv")
+#
+5. # for the parasite, which includes several guilds
+
+pa=data.frame(c("animal_parasite","lichen_parasite","protistan_parasite","algal_parasite" ))
+names(pa)="pa"
+5.
+m=subset_taxa(rare_all,ta2%in%pa$pa)# select a guild
+m<- subset_samples(m, !is.na(lon) & !is.na(lat))
+m<- subset_taxa(m, taxa_sums(m) > 0)
+m<- subset_samples(m, sample_sums(m) > 0)
+
+
+# add a plotID for the data
+
+d=sample_data(m)
+table(d$Project)# the first 908 rows are dob sites while the remaining 5470 rows are NEON sites
+d1=data.frame(d[,c("geneticSampleID","Site")])
+plotID=substr(d1$geneticSampleID,1,8)
+d1=cbind(d1,plotID)
+iddob=d1$Site[1:table(d$Project)[[1]]]#a site corresponds to a plot
+idneon=d1$plotID[(table(d$Project)[[1]]+1):sum(table(d$Project))]# a plotID corresponds to a plot
+plotIDM=data.frame(c(iddob,idneon))
+
+names(plotIDM)="plotIDM"# the plotID used for the SAR
+row.names(plotIDM)=row.names(sample_data(m))#an important step to merge the data
+plotIDM=sample_data(plotIDM)
+d<- merge_phyloseq(m, plotIDM)# merge the new plotid with the initial data 
+
+a1= sample_data(d)
+a1=unique(a1$plotIDM)
+
+set.seed(10102)
+times=30
+power.z <- vector("list", length(a1))
+
+for (i in 1:length(a1))
+{
+  cat('\r',paste(paste0(rep("*", round(i/ 1, 0)), collapse = ''), i, collapse = ''))# informs the processing
+  data_sub <- subset_samples(d, plotIDM==a1[i])
+  dim1 <- dim(otu_table(data_sub)) # the number of samples in one site
+  if (dim1[1] >= 3)# we can construct a linear regression model with at least three cores
+  {
+    cl <- makeCluster(3)
+    registerDoParallel(cl)
+    power.z[[i]] <- foreach(i = 1:times, .combine = "cbind", .packages = c("phyloseq")) %dopar%{
+      species <- vector(length = dim1[1]) # create a vector to save diversity
+      for (j in 1:dim1[1]) 
+      { 
+        # randomly sample j samples in the plot
+        flag <- rep(FALSE, dim1[1])
+        flag[sample(1:dim1[1], j)] <- TRUE
+        temp <- merge_samples(data_sub, flag, function(x) mean(x, na.rm = TRUE)) # the j samples aggregated by mean
+        species[j] <- sum(otu_table(temp)["TRUE"] > 0)
+      }
+      ex <- as.data.frame(cbind("A"=c(1:dim1[1]),species))
+      temp<- summary(lm(log(species)~log(A),ex))[["coefficients"]]# extract the estimated c and z values
+      return(c(temp[2,1], temp[1,1]))
+    }
+    stopCluster(cl)
+  }
+  else
+  {
+    power.z[[i]]=matrix(10,nrow=2,ncol = dim1[1])#the number 10 is randomly selected to create a matrix for the plots with < 3 cores to avoid NULL output
+  }
+}
+
+para_z=matrix(nrow=length(a1),ncol=30)# get the 30 simulated z values for all the plots
+for(i in 1:length(a1)){
+  para_z[i,]=power.z[[i]][1,]
+}
+
+para_z=data.frame(a1,para_z)
+write.csv(para_z,"para_z.ranall.csv")
+
+para_c=matrix(nrow=length(a1),ncol=30)# get the 30 simulated c values for the neon site
+for(i in 1:length(a1)){
+  para_c[i,]=power.z[[i]][2,]
+}
+
+para_c=data.frame(a1,para_c)
+write.csv(para_c,"para_c.ranall.csv")
 #
 
