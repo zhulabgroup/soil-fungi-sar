4a3d96d979526e1ae0ccdffc9aada57dbb326b91
wenqisysu <wenqil@umich.edu>
2023-10-23 13:01:40 -0400

    compare the z values among land cover types

--- compare the z values among land cover types.R
+++ compare the z values among land cover types.R
@@ -38,10 +38,23 @@ source("http://aoki2.si.gunma-u.ac.jp/R/src/tukey.R", encoding="euc-jp")
 k=aggregate(z~type,data=comp_vege,FUN=mean)
 od=k[order(k$z),]# with the increase trend to display the box plots
 comp_vege$type=factor(comp_vege$type,levels=od$type)
-ggboxplot(comp_vege,x="type",y="z",fill = "type")+
+
+a=ggboxplot(comp_vege,x="type",y="z",fill = "type",outlier.shape = NA)+
   xlab("")+
   theme(legend.position = "bottom", legend.text = element_text(size=14), text = element_text(size=15), axis.text.x = element_blank(),axis.title.y = element_text(face= "italic",size=20),axis.title.x = element_text(size=20),axis.ticks.x = element_blank())+
   ylab("z")+
-  guides(fill=guide_legend(nrow=4, byrow=TRUE))+# make four rows for the legend
+  theme(legend.position = c(0.45,0.7858), legend.text = element_text(size=14), text = element_text(size=15), axis.text.x = element_blank(),axis.title.y = element_text(face= "italic",size=20),axis.title.x = element_text(size=20),axis.ticks.x = element_blank())+
+  guides(fill=guide_legend(nrow=7, byrow=TRUE))+# make four rows for the legend
   geom_hline(yintercept =0.7559199,linetype="dashed",color="red" )+
-  scale_fill_discrete("",breaks=od$type,labels=c("dwarfScrub(N=3)" , "emergentHerbaceousWetlands(N=4)", "grasslandHerbaceous(N=64)" , "pastureHay(N=17)", "shrubScrub(N=51)" , "evergreenForest(N=93)","woodyWetlands(N=29)","deciduousForest(N=96)",  "cultivatedCrops(N=22)"  ,  "mixedForest(N=20)" , "sedgeHerbaceous(N=4)"   ))
+  scale_fill_manual("",breaks=od$type,values=c("chocolate1","gray","cadetblue1","lavender","greenyellow","mediumseagreen","cornsilk","tan","wheat","gold","mediumpurple"),labels=c("dwarfScrub(N=3)" , "emergentHerbaceous\nWetlands(N=4)", "grassland\nHerbaceous(N=64)" , "pastureHay(N=17)", "shrubScrub(N=51)" , "evergreenForest(N=93)","woodyWetlands(N=29)","deciduousForest(N=96)",  "cultivatedCrops(N=22)"  ,  "mixedForest(N=20)" , "sedgeHerbaceous(N=4)"   ))+
+  annotate("text",x=1,y=0.96,label="b",size=6)+
+  annotate("text",x=2,y=0.94,label="b",size=6)+
+  annotate("text",x=3,y=1.05,label="b",size=6)+
+  annotate("text",x=4,y=1.06,label="b",size=6)+
+  annotate("text",x=5,y=1.07,label="b",size=6)+
+  annotate("text",x=6,y=1.15,label="a",size=6)+
+  annotate("text",x=7,y=1.13,label="a",size=6)+
+  annotate("text",x=8,y=1.08,label="a",size=6)+
+  annotate("text",x=9,y=1.21,label="a",size=6)+
+  annotate("text",x=10,y=1.11,label="a",size=6)+
+  annotate("text",x=11,y=1.5,label="ab",size=6)
