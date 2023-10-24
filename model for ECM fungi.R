ce75cd770d5d4aa09b371fca6f02b04532fe8691
wenqisysu <wenqil@umich.edu>
2023-10-24 16:46:59 -0400

    ecm

--- model for ECM fungi.R
+++ model for ECM fungi.R
@@ -1,4 +1,6 @@
 # model for ECM
+
+
 a=list()
 for (i in 1:dim(ecm_c_ranall)[1])
 {
@@ -16,6 +18,29 @@ for(i in 2:dim(ecm_c_ranall)[1])
 plotID=rep(ecm_c_ranall$a1,each=30)
 
 ecm_c_ranall_30=cbind(plotID,b)# for each plot,with 30 estimated z values
+
+# add the z value
+
+a=list()
+for (i in 1:dim(ecm_z_ranall)[1])
+{
+  d=data.frame(t(ecm_z_ranall[i,3:32]))
+  names(d)="id"
+  a[[i]]=d
+}
+
+b=a[[1]]
+for(i in 2:dim(ecm_z_ranall)[1])
+{
+  b=rbind(b,a[[i]])
+}
+
+
+
+acm_z_ranall_30=b# for each plot,with 30 estimated z values
+ecm_z_ranall_30=cbind(plotID,b)# for each plot,with 30 estimated z values
+names(ecm_z_ranall_30)[2]="z"
+# cbind the c and z value
 
 ecm_model=cbind(ecm_c_ranall_30,ecm_z_ranall_30["z"])
 names(ecm_model)[2]="logc"
@@ -38,7 +63,14 @@ ggcorrplot(cor(ecm_model1[,c(2,3,5:27)]), hc.order = TRUE, type = "lower", lab =
 #bold (removed)and soil OC
 #bio1 and bio4(removed)
 3
-# build a model for the ecm guild,with 104 plots included, soil pH and fundiversity
+# build a model for the ecm guild,with 104 plots included, soil pH and fundive
 
 mod=lmer(z ~ logc+organicCPercent + ph+ nitrogen+ sand +bio2 +bio8+ bio18 +bio12+ bio15  +spei+ rich+funrich +bio1 +fine+ d15N +d13C +  rootc +rootcn +(1 |siteIDD/plotID),data=ecm_model1)
-##
+ecm_effect=summary(mod)
+ecm_effect=data.frame(ecm_effect$coefficients)
+sig=ecm_effect$Pr...t..
+sig[sig>0.05]="no"
+sig[sig<0.05]="sig"
+ecm_effect=cbind(ecm_effect,sig)
+
+
