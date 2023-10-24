db7de701a983db278b4feea7a9413373d2465203
wenqisysu <wenqil@umich.edu>
2023-10-23 21:28:18 -0400

    soil saprotroph

---
+++ model for soilsap.R
@@ -0,0 +1,42 @@
+# model for soilsap
+a=list()
+for (i in 1:dim(soilsap_c_ranall)[1])
+{
+  d=data.frame(t(soilsap_c_ranall[i,3:32]))
+  names(d)="id"
+  a[[i]]=d
+}
+
+b=a[[1]]
+for(i in 2:dim(soilsap_c_ranall)[1])
+{
+  b=rbind(b,a[[i]])
+}
+
+plotID=rep(soilsap_c_ranall$a1,each=30)
+
+soilsap_c_ranall_30=cbind(plotID,b)# for each plot,with 30 estimated z values
+
+soilsap_model=cbind(soilsap_c_ranall_30,soilsap_z_ranall_30["z"])
+names(soilsap_model)[2]="logc"
+soilsap_model=merge(soilsap_model,model_var,by="plotID")
+soilsap_model=subset(soilsap_model,siteIDD!="GUAN"&z<10&fine>0&rootc>0&rich>0)# only 104 plots from 33 sites
+
+
+
+#standardized data
+soilsap_model[,c(2,5:27)]=apply(soilsap_model[,c(2,5:27)],2,range01)
+# colinearity
+ggcorrplot(cor(soilsap_model[,c(2,3,5:27)]), hc.order = TRUE, type = "lower", lab = TRUE)#
+#bold was related with soil OC and was excluded,cec was related to soil OC and was removed
+#bio4 was related with bio1 and was removed
+#coarse (removed) was related with fine and was removed
+#cec(removed) and soil OC
+#root cn and rootn(removed)
+#bold (removed)and soil OC
+#bio1 and bio4(removed)
+3
+# build a model for the soilsap guild,with 104 plots included, soil pH and fundiversity
+
+mod=lmer(z ~ logc+organicCPercent + ph+ nitrogen+ sand +bio2 +bio8+ bio18 +bio12+ bio15  +spei+ rich+funrich +bio1 +fine+ d15N +d13C +rootc +rootcn +(1 |siteIDD/plotID),data=soilsap_model)
+##
