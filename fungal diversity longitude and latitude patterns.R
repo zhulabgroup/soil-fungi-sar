ff665dfc660191ef726cf04ef208fbc3b1806499
wenqisysu <wenqil@umich.edu>
2023-10-25 17:31:00 -0400

    a

--- fungal diversity longitude and latitude patterns.R
+++ fungal diversity longitude and latitude patterns.R
@@ -1,12 +1,10 @@
 # look at the relationship between fungal diversity and environmental variables
-
-
 d <- sample_data(rare_all)
 table(d$Project) # the first 908 rows are dob sites with the remaining 5470 being NEON sites
 d1 <- data.frame(d[, c("geneticSampleID", "Site")])
 plotID <- substr(d1$geneticSampleID, 1, 8)
 d1 <- cbind(d1, plotID)
-iddob <- d1$Site[1:908] # a site correspondes to a plot
+iddob <- d1$Site[1:908] # a site correspond to a plot
 idneon <- d1$plotID[909:6378] # an unique plotID corresponds to a plot
 plotIDM <- data.frame(c(iddob, idneon))
 names(plotIDM) <- "plotIDM" # the plot id used for the SAR
@@ -28,11 +26,6 @@ for (i in 1:length(a1)) {
   otu <- otu_table(neon_sub)
   rich[i] <- table(apply(otu, 2, sum) > 0)[[2]] # richness within a specific plot for a site
 }
-
-
-
-
-
 rich_plot <- data.frame(a1, rich)
 siteid <- substr(rich_plot$a1, 1, 4)
 
