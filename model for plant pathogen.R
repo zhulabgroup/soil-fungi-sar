59a2efc5692c935a1d4099ed654eba0f396dfd40
wenqisysu <wenqil@umich.edu>
2023-11-14 13:28:05 -0500

    a

--- model for plant pathogen.R
+++ model for plant pathogen.R
@@ -14,10 +14,28 @@ for (i in 2:dim(plapat_c_ranall)[1])
 }
 
 plotID <- rep(plapat_c_ranall$a1, each = 30)
-
 plapat_c_ranall_30 <- cbind(plotID, b) # for each plot,with 30 estimated z values
+#add z
+a <- list()
+for (i in 1:dim(plapat_z_ranall)[1])
+{
+  d <- data.frame(t(plapat_z_ranall[i, 3:32]))
+  names(d) <- "id"
+  a[[i]] <- d
+}
+
+b <- a[[1]]
+for (i in 2:dim(plapat_z_ranall)[1])
+{
+  b <- rbind(b, a[[i]])
+}
+
+
+plapat_z_ranall_30 <- b # for each plot,with 30 estimated z values
+names(plapat_z_ranall_30)="z"
 
 plapat_model <- cbind(plapat_c_ranall_30, plapat_z_ranall_30["z"])
+
 names(plapat_model)[2] <- "logc"
 plapat_model <- merge(plapat_model, model_var, by = "plotID")
 plapat_model <- subset(plapat_model, siteIDD != "GUAN" & z < 10 & fine > 0 & rootc > 0 & rich > 0) # only 104 plots from 33 sites
@@ -40,3 +58,13 @@ ggcorrplot(cor(plapat_model[, c(2, 3, 5:27)]), hc.order = TRUE, type = "lower", 
 
 mod <- lmer(z ~ logc + organicCPercent + ph + nitrogen + sand + bio2 + bio8 + bio18 + bio12 + bio15 + spei + rich + funrich + bio1 + fine + d15N + d13C + rootc + rootcn + (1 | siteIDD / plotID), data = plapat_model)
 ##
+# #
+mod <- lmer(z ~ logc + organicCPercent + ph + nitrogen + sand + bio2 + bio8 + bio18 + bio12 + bio15 + spei + rich + funrich + bio1 + fine + d15N + d13C + rootc + rootcn + (1 | plotID), data = plapat_model)
+plapat_effect_plotran <- summary(mod)
+plapat_effect_plotran <- data.frame(plapat_effect_plotran$coefficients)
+sig <- round(plapat_effect_plotran$Pr...t..,3)
+sig[sig > 0.05] <- "no"
+sig[sig < 0.05] <- "sig"
+
+plapat_effect_plotran <- cbind(plapat_effect_plotran, sig)
+
