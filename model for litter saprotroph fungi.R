64b7b722f6f035f2ad168eb04d57c08aae442615
wenqisysu <wenqil@umich.edu>
2023-12-02 22:47:43 -0500

    excluding the root traits

--- model for litter saprotroph fungi.R
+++ model for litter saprotroph fungi.R
@@ -37,12 +37,12 @@ names(litsap_z_ranall_30)="z"
 litsap_model <- cbind(litsap_c_ranall_30, litsap_z_ranall_30["z"])
 names(litsap_model)[2] <- "logc"
 litsap_model <- merge(litsap_model, model_var, by = "plotID")
-litsap_model <- subset(litsap_model, siteIDD != "GUAN" & z < 10 & fine > 0 & rootc > 0 & rich > 0) # only 104 plots from 33 sites
+litsap_model <- subset(litsap_model, siteIDD != "GUAN" & z < 10 & fine > 0 & rootc > 0 & richness > 0) # only 104 plots from 33 sites
 
-
+litsap_model  <- cbind(litsap_model, c = 2.71828^litsap_model $logc)
 
 # standardized data
-litsap_model[, c(2, 5:27)] <- apply(litsap_model[, c(2, 5:27)], 2, range01)
+litsap_model[, c( 5:28)] <- apply(litsap_model[, c(5:28)], 2, range01)
 # colinearity
 ggcorrplot(cor(litsap_model[, c(2, 3, 5:27)]), hc.order = TRUE, type = "lower", lab = TRUE) #
 # bold was related with soil OC and was excluded,cec was related to soil OC and was removed
@@ -55,6 +55,14 @@ ggcorrplot(cor(litsap_model[, c(2, 3, 5:27)]), hc.order = TRUE, type = "lower", 
 3
 # build a model for the litsap guild,with 104 plots included, rich and d13
 
-mod <- lmer(z ~ logc + organicCPercent + ph + nitrogen + sand + bio2 + bio8 + bio18 + bio12 + bio15 + spei + rich + funrich + bio1 + fine + d15N + d13C + rootc + rootcn + (1 | siteIDD / plotID), data = litsap_model)
+mod <- lmer(z ~ c + organicCPercent + ph + nitrogen + sand + bio2 + bio8 + bio18 + bio12 + bio15 + spei + richness + funrich + bio1 + fine + d15N + d13C + rootc + rootcn + (1 | siteIDD / plotID), data = litsap_model)
+
+step(mod)
+
+mod_litsap=lmer(z ~ c + funrich + (1 | siteIDD/plotID),data=litsap_model)
+
+p5=plot_model(mod_litsap,axis.labels = c("Fun.rich"),colors=c("blue","red"),rm.terms = "c",title="Lit.sap. (N=104)",axis.lim = c(-1,1))
+
+
 ##
 mod <- lmer(z ~ logc + organicCPercent + ph + nitrogen + sand + bio2 + bio8 + bio18 + bio12 + bio15 + spei + rich + funrich + bio1 + fine + d15N + d13C + rootc + rootcn + (1 | plotID), data = litsap_model)
