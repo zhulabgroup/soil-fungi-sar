335d1159156befbc05a2bae45c348e81fab34de8
wenqisysu <wenqil@umich.edu>
2024-03-03 10:53:55 -0500

    modify figures

--- model_nested.R
+++ model_nested.R
@@ -1,8 +1,17 @@
 # model selection for each model
-#site and plot are nested
+#for the prestented figures, site and plot are nested in the random effect setting
 library(sjPlot)
 library(ggplot2)
 library(effects)
+library(lme4)
+library(lmerTest)
+
+# function for data standardization
+range01 <- function(x) ## to
+{
+  return((x - min(x)) / (max(x) - min(x)))
+}
+
 
 1. # climate and soil model:both dob and neon sites were included, here only the plotID was treated as a random effect
 mode.data1 <- model_data[, c(1:18,20)] # GUAN don't have soil variables and will be excluded(possibly this site is out of place)
@@ -14,19 +23,12 @@ cor(mode.data1[, 4:19])
 ggcorrplot(cor(mode.data1[, 4:19]), hc.order = TRUE, type = "lower", lab = TRUE) #
 # bold was related with soil C, and MAT;cec was related with soil C; I decided to exclude cec and bold
 # build model with plot included as the random effect
-# standardized the data.
-
-range01 <- function(x) ## to
-{
-  return((x - min(x)) / (max(x) - min(x)))
-}
 
 mode.data1[, 4:19] <- apply(mode.data1[, 4:19], 2, range01) %>% data.frame()
 
-# plot and site included as the random effect, here some sites have only samples from the same plot
+# plot and site included as nested random effects, here some sites have only samples from the same plot
 mod <- lmer(z ~ c + organicCPercent + ph + nitrogen + sand + bio2 + bio8 + bio18 + bio4 + bio12 + bio15 + spei + funrich + bio1 + (1 | siteIDD/plotID), data = mode.data1)
 step(mod)
-
 
 #best model
 mod_nest=lmer(z ~ c + nitrogen + sand + bio2 + bio12 + bio15 + funrich + (1 | siteIDD/plotID),data=mode.data1)
@@ -43,13 +45,11 @@ axis.textsize.y = 1,
 title.size = 2,
 title.align= "center")  #To change y axis text size
 
-plot_model(mod_nest,axis.labels=c("Fun.rich","Pre.seas.","MAP","MDR","Sand","SoilN","c"),color=c("blue","red"),rm.terms = "c",title="Climate+Soil (N=483)")
+p1=plot_model(mod_nest,axis.labels=c("Fun.rich","Pre.seas.","MAP","MDR","Sand","SoilN","c"),color=c("blue","red"),rm.terms = "c",title="Climate+Soil \n(N=483)",dot.size = 4)
 
 ## tables
 tab_model(mod_nest)
-
 # predicting the effects of specific variables
-
 effects_fun <- effects::effect(term= "funrich", mod= mod_nest)
 summary(effects_fun) 
 x_fun <- as.data.frame(effects_fun)
@@ -76,17 +76,9 @@ ggplot() +
   guides(color=FALSE)
 
 
-## if we dropped the c, we got the best model
-
+#if we dropped the c, we got the best model
 mod <- lmer(z ~   bio4 + bio12 + (1 | siteIDD/plotID), data = mode.data1)
 step(mod)
-
-
-
-
-
-
-
 
 
 2. # climate, soil and plant diversity model
@@ -115,9 +107,9 @@ set_theme(base = theme_classic(), #To remove the background color and the grids
           title.size = 2,
           title.align= "center")  #To change y axis text size
 
-plot_model(mod_nestCSP,axis.labels=c("Fun.rich","Pre.seas.","MAP","Mean\n diurnal \n range","Sand","Pla.rich","SoilN","c"),colors=c("blue","red"),rm.terms = "c",title="Climate+Soil+Plant (N=438)")
+p2=plot_model(mod_nestCSP,axis.labels=c("Fun.rich","Pre.seas.","MAP","MDR","Sand","Pla.rich","SoilN","c"),colors=c("blue","red"),rm.terms = "c",title="Climate+Soil+Plant \n(N=438)",dot.size = 4)
 
-# to see the effect of plant richnes
+# to see the effect of plant richness
 
 effects_richness <- effects::effect(term= "richness", mod= mod_nestCSP)
 summary(effects_richness) 
@@ -132,10 +124,6 @@ ggplot() +
   guides(color=FALSE)
 
 tab_model(mod_nestCSP)
-
-
-
-
 
 
 3.# climate, soil, plant and root model
@@ -156,15 +144,16 @@ mode.data3[, 3:26] <- apply(mode.data3[, 3:26], 2, range01) %>% data.frame()
 # root n-root cn
 
 # site and plot are nested, only funrich showed a significant effect with plant rich showing a marginal effect
-mod <- lmer(z ~ organicCPercent + c + ph + nitrogen + richness + sand + bio2 + bio8 + bio18 + +bio12 + bio15 + spei + funrich + bio1 + fine + d13C + rootc + rootcn + (1 | siteIDD/plotID), data = mode.data3)
 
+mod <- lmer(z ~ organicCPercent + c + ph + nitrogen + richness + sand + bio2 + bio8 + bio18 + +bio12 + bio15 + spei + funrich + bio1 + fine + d13C + rootc + rootcn + (1 | siteIDD/plotID), data = mode.data3)
 step(mod)
+
 mod_nestCSPR=lmer(z ~c + richness + funrich + bio1 + (1 | siteIDD/plotID),data=mode.data3)
 # create the effect size plot
 
 plot_model(mod_nestCSPR)
 
-plot_model(mod_nestCSPR,axis.labels=c("MAT","Fun.rich","Pla.rich"),color=c("blue","red"),rm.terms = "c",title="Climate+Soil+Plant+Root (N=104)",dot.size = 5)
+p3=plot_model(mod_nestCSPR,axis.labels=c("MAT","Fun.rich","Pla.rich"),color=c("blue","red"),rm.terms = "c",title="Climate+Soil+Plant+Root \n(N=104)",dot.size = 5)
 
 effects_bio1 <- effects::effect(term= "bio1", mod= mod_nestCSPR)
 summary(effects_bio1) 
