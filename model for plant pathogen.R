c7c7ab0413077d95de76756509cf8cffc10fec76
wenqisysu <wenqil@umich.edu>
2024-03-04 10:06:31 -0500

    add the richness model

--- model for plant pathogen.R
+++ model for plant pathogen.R
@@ -1,4 +1,10 @@
 # model for plapat
+
+range01 <- function(x) 
+  {
+ return((x - min(x)) / (max(x) - min(x)))
+}
+  
 a <- list()
 for (i in 1:dim(plapat_c_ranall)[1])
 {
@@ -32,6 +38,7 @@ for (i in 2:dim(plapat_z_ranall)[1])
 
 
 plapat_z_ranall_30 <- b # for each plot,with 30 estimated z values
+
 names(plapat_z_ranall_30)="z"
 
 plapat_model <- cbind(plapat_c_ranall_30, plapat_z_ranall_30["z"])
@@ -41,9 +48,12 @@ plapat_model <- merge(plapat_model, model_var, by = "plotID")
 plapat_model <- subset(plapat_model, siteIDD != "GUAN" & z < 10 & fine > 0 & rootc > 0 & richness > 0) # only 104 plots from 33 sites
 
 plapat_model <- cbind(plapat_model, c = 2.71828^plapat_model$logc)
+plapat_model_rich <- subset(plapat_model, siteIDD != "GUAN" & z < 10 & richness > 0) # only 104 plots from 33 sites
+
 
 # standardized data
 plapat_model[, c(5:28)] <- apply(plapat_model[, c(5:28)], 2, range01)
+plapat_model_rich[, c(5:28)] <- apply(plapat_model_rich[, c(5:28)], 2, range01)
 # colinearity
 ggcorrplot(cor(plapat_model[, c(2, 3, 5:27)]), hc.order = TRUE, type = "lower", lab = TRUE) #
 # bold was related with soil OC and was excluded,cec was related to soil OC and was removed
@@ -62,10 +72,21 @@ step(mod)
 
 mod_plapat=lmer(z ~ c + organicCPercent + bio15 + funrich + fine + (1 | siteIDD/plotID),data=plapat_model)
 
-plot_model(mod_plapat)
+mod_plapat=lmer(z ~ c + organicCPercent + sand + bio15 + spei + funrich + fine + (1 | siteIDD/plotID),data=plapat_model)
+
+p4=plot_model(mod_plapat,axis.labels = c(expression("Root"["fmass"]),"Fun.rich","Spei","Pre.seas","Sand","SoilC"),colors=c("blue","red"),rm.terms = "c",title="Pla.patho. (N=103)")
+
+# when root traits were excluded
+mod <- lmer(z ~ c + organicCPercent + ph + nitrogen + sand + bio2 + bio8 + bio18 + bio12 + bio15 + spei + richness + funrich + bio1  + (1 | siteIDD / plotID), data = plapat_model_rich)
+
+step(mod)
+
+mod_plapat_rich <- lmer(z ~ c + organicCPercent + sand + bio15 + spei + richness + funrich + (1 | siteIDD/plotID), data = plapat_model_rich)
+
+p4=plot_model(mod_plapat_rich,axis.labels = c("Fun.rich", "Pla.rich","Spei","Pre.seas","Sand","SoilC"),colors=c("blue","red"),rm.terms = "c",title="Pla.patho. (N=103)")
 
 
-p4=plot_model(mod_plapat,axis.labels = c(expression("Root"["fmass"]),"Fun.rich","Pre.seas","SoilC"),rm.terms = "c",title="Pla.patho. (N=87)")
+
 
 ##
 # #
