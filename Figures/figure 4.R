cf2ac2186d7f8437404ba96f7eea0fdce68b865d
wenqisysu <wenqil@umich.edu>
2024-03-16 16:45:42 -0400

    remove three land cover types because of few plots

--- Figures/figure 4.R
+++ Figures/figure 4.R
@@ -3,6 +3,8 @@
 # com_guild#each fungal guil has 30 estimated z values
 
 1. # test the homogeneity of the variance
+# excluded three land cover types that have less than 5 plots 
+comp_vege=subset(comp_vege,type!="dwarfScrub"&type!="sedgeHerbaceous"&type!="emergentHerbaceousWetlands")
 leveneTest(z ~ type, data = comp_vege)
 oneway.test(z ~ type, data = comp_vege, na.action = na.omit, var.equal = FALSE)
 tukey(comp_vege$z, comp_vege$type, method = "G")
@@ -12,25 +14,34 @@ source("http://aoki2.si.gunma-u.ac.jp/R/src/tukey.R", encoding = "euc-jp")
 k <- aggregate(z ~ type, data = comp_vege, FUN = mean)
 od <- k[order(k$z), ] # with the increase trend to display the box plots
 comp_vege$type <- factor(comp_vege$type, levels = od$type)
-a <- ggboxplot(comp_vege, x = "type", y = "z", fill = "type", outlier.shape = NA) +
+
+aa=ggboxplot(comp_vege, x = "type", y = "z", fill = "type", outlier.shape = NA) +
   xlab("") +
-  theme(legend.position = "bottom", legend.text = element_text(size = 14), text = element_text(size = 15), axis.text.x = element_blank(), axis.title.y = element_text(face = "italic", size = 20), axis.title.x = element_text(size = 20), axis.ticks.x = element_blank()) +
-  ylab("z") +
-  theme(legend.position = c(0.45, 0.8058), legend.text = element_text(size = 14), text = element_text(size = 15), axis.text.x = element_blank(), axis.title.y = element_text(face = "italic", size = 20), axis.title.x = element_text(size = 20), axis.ticks.x = element_blank()) +
+  ylab("z")+
+  theme(legend.position = c(0.48, 0.8058), 
+        legend.text = element_text(size = 14),
+        text = element_text(size = 15), 
+        axis.text.x = element_blank(),
+        axis.title.y = element_text(face = "italic", size = 20), 
+        axis.title.x = element_text(size = 20), 
+        axis.ticks.x = element_blank()) +
   guides(fill = guide_legend(nrow = 7, byrow = TRUE)) + # make four rows for the legend
-  geom_hline(yintercept = 0.7559199, linetype = "dashed", color = "red") +
-  scale_fill_manual("", breaks = od$type, values = c("chocolate1", "gray", "cadetblue1", "lavender", "greenyellow", "mediumseagreen", "cornsilk", "tan", "wheat", "gold", "mediumpurple"), labels = c("dwarfScrub(N=3)", "emergentHerbaceous\nWetlands(N=4)", "grassland\nHerbaceous(N=64)", "pastureHay(N=17)", "shrubScrub(N=51)", "evergreenForest(N=93)", "woodyWetlands(N=29)", "deciduousForest(N=96)", "cultivatedCrops(N=22)", "mixedForest(N=20)", "sedgeHerbaceous(N=4)")) +
-  annotate("text", x = 1, y = 0.96, label = "b", size = 6) +
-  annotate("text", x = 2, y = 0.94, label = "b", size = 6) +
-  annotate("text", x = 3, y = 1.05, label = "b", size = 6) +
-  annotate("text", x = 4, y = 1.06, label = "b", size = 6) +
-  annotate("text", x = 5, y = 1.07, label = "b", size = 6) +
-  annotate("text", x = 6, y = 1.15, label = "a", size = 6) +
-  annotate("text", x = 7, y = 1.13, label = "a", size = 6) +
-  annotate("text", x = 8, y = 1.08, label = "a", size = 6) +
-  annotate("text", x = 9, y = 1.21, label = "a", size = 6) +
-  annotate("text", x = 10, y = 1.11, label = "a", size = 6) +
-  annotate("text", x = 11, y = 1.5, label = "ab", size = 6)
+  geom_hline(yintercept = 0.7559199,
+             linetype = "dashed", 
+             color = "red")+ 
+  scale_fill_manual("", breaks = od$type, 
+  values = c(  "cadetblue1", "tan", "greenyellow", "mediumseagreen", "wheat", "cornsilk","lavender","mediumpurple"), 
+  labels = c( "grassland\nHerbaceous(N=64)", "pastureHay(N=17)", "shrubScrub(N=51)", "evergreenForest(N=93)", "woodyWetlands(N=29)", "deciduousForest(N=96)", "cultivatedCrops(N=22)", "mixedForest(N=20)"))+
+  annotate("text", x = 1, y = 1.03, label = "e", size = 6) +
+  annotate("text", x = 2, y = 1.05, label = "e", size = 6) +
+  annotate("text", x = 3, y = 1.07, label = "e", size = 6) +
+  annotate("text", x = 4, y = 1.15, label = "d", size = 6) +
+  annotate("text", x = 5, y = 1.13, label = "ce", size = 6) +
+  annotate("text", x = 6, y = 1.125, label = "bcd", size = 6) +
+  annotate("text", x = 7, y = 1.218, label = "b", size = 6) +
+  annotate("text", x = 8, y = 1.1108, label = "ace", size = 6)
+
+
 
 3. # test the homogeneity of the variance
 leveneTest(z ~ guild, data = com_guild) # testing variance homogenety, unblanced
