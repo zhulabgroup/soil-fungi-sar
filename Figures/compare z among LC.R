b8e399e5132aa0b74f8fb37536f5be4566ccd403
wenqisysu <wenqil@umich.edu>
2024-03-18 22:06:18 -0400

    edits

--- Figures/compare z among LC.R
+++ Figures/compare z among LC.R
@@ -20,6 +20,7 @@ gu=unique(d1$guild)
 
 source("http://aoki2.si.gunma-u.ac.jp/R/src/tukey.R", encoding = "euc-jp")
 
+
 comp=list()
 od=list()
 ann=list()# to test if species turn over rate differ among land cover types
@@ -76,7 +77,13 @@ p2=ggboxplot(data2, x = "type", y = "z", fill = "type", outlier.shape = NA) +
   xlab("") +
   theme(legend.position = "bottom", legend.text = element_text(size = 14), text = element_text(size = 15), axis.text.x = element_blank(), axis.title.y = element_text(face = "italic", size = 20), axis.title.x = element_text(size = 20), axis.ticks.x = element_blank()) +
   ylab("z") +
-  theme(legend.position = c(0.45, 0.7558), legend.text = element_text(size = 14), text = element_text(size = 15), axis.text.x = element_blank(), axis.title.y = element_text(face = "italic", size = 20), axis.title.x = element_text(size = 20), axis.ticks.x = element_blank()) +
+  theme(legend.position = c(0.45, 0.7558), 
+        legend.text = element_text(size = 14), 
+        text = element_text(size = 15), 
+        axis.text.x = element_blank(), 
+        axis.title.y = element_text(face = "italic", size = 20), 
+        axis.title.x = element_text(size = 20), 
+        axis.ticks.x = element_blank()) +
   guides(fill = guide_legend(nrow = 7, byrow = TRUE)) + # make four rows for the legend
   geom_hline(yintercept = 0.7559199, linetype = "dashed", color = "red") +
   annotate("text", x = 1, y = 1.25, label = "e", size = 6) +
