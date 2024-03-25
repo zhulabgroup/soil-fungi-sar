06543b1f627b29dcd56b85ce5cb24417583f44b1
wenqisysu <wenqil@umich.edu>
2024-03-25 09:04:52 -0400

    updated z value for epiphyte

--- Figures/figure 4.R
+++ Figures/figure 4.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+library(ggpubr)
 # date required
 # comp_vege#each plot has 30 estimated z values
 # com_guild#each fungal guil has 30 estimated z values
@@ -8,6 +10,7 @@ comp_vege=subset(comp_vege,type!="dwarfScrub"&type!="sedgeHerbaceous"&type!="eme
 leveneTest(z ~ type, data = comp_vege)
 oneway.test(z ~ type, data = comp_vege, na.action = na.omit, var.equal = FALSE)
 tukey(comp_vege$z, comp_vege$type, method = "G")
+
 source("http://aoki2.si.gunma-u.ac.jp/R/src/tukey.R", encoding = "euc-jp")
 
 2. # create the plot
@@ -32,43 +35,46 @@ aa=ggboxplot(comp_vege, x = "type", y = "z", fill = "type", outlier.shape = NA) 
   scale_fill_manual("", breaks = od$type, 
   values = c(  "cadetblue1", "tan", "greenyellow", "mediumseagreen", "wheat", "cornsilk","lavender","mediumpurple"), 
   labels = c( "grassland\nHerbaceous(N=64)", "pastureHay(N=17)", "shrubScrub(N=51)", "evergreenForest(N=93)", "woodyWetlands(N=29)", "deciduousForest(N=96)", "cultivatedCrops(N=22)", "mixedForest(N=20)"))+
-  annotate("text", x = 1, y = 1.03, label = "e", size = 6) +
-  annotate("text", x = 2, y = 1.05, label = "e", size = 6) +
-  annotate("text", x = 3, y = 1.07, label = "e", size = 6) +
-  annotate("text", x = 4, y = 1.15, label = "d", size = 6) +
-  annotate("text", x = 5, y = 1.13, label = "ce", size = 6) +
-  annotate("text", x = 6, y = 1.125, label = "bcd", size = 6) +
-  annotate("text", x = 7, y = 1.218, label = "b", size = 6) +
-  annotate("text", x = 8, y = 1.1108, label = "ace", size = 6)
+  annotate("text", x = 1, y = 1.03, label = "c", size = 6) +
+  annotate("text", x = 2, y = 1.05, label = "c", size = 6) +
+  annotate("text", x = 3, y = 1.07, label = "c", size = 6) +
+  annotate("text", x = 4, y = 1.15, label = "b", size = 6) +
+  annotate("text", x = 5, y = 1.13, label = "ab", size = 6) +
+  annotate("text", x = 6, y = 1.125, label = "ab", size = 6) +
+  annotate("text", x = 7, y = 1.218, label = "ab", size = 6) +
+  annotate("text", x = 8, y = 1.1108, label = "a", size = 6)
 
-
+# load()
 
 3. # test the homogeneity of the variance
 leveneTest(z ~ guild, data = com_guild) # testing variance homogenety, unblanced
 oneway.test(z ~ guild, data = com_guild, na.action = na.omit, var.equal = FALSE)
 source("http://aoki2.si.gunma-u.ac.jp/R/src/tukey.R", encoding = "euc-jp")
+
 tukey(com_guild$z, com_guild$guild, method = "G") # multiple comparison with 'Games.Howell' Post-hoc Test
 
-4. # creat a plot
+4. # create a plot
 k <- aggregate(z ~ guild, data = com_guild, FUN = mean)
 od1 <- k[order(k$z), ] # with the increase trend to display the box plots
+
 com_guild$guild <- factor(com_guild$guild, levels = od1$guild)
 
-b <- ggboxplot(com_guild, x = "guild", y = "z", fill = "guild", outlier.colour = "gray", outlier.shape = NA) +
+ bb=ggboxplot(com_guild, x = "guild", y = "z", fill = "guild", outlier.colour = "gray", outlier.shape = NA) +
   guides(fill = guide_legend(nrow = 4, byrow = TRUE)) +
   geom_hline(yintercept = 0.787, linetype = "dashed", color = "red") +
   xlab("") +
   theme(legend.position = c(0.51, 0.85), legend.text = element_text(size = 14), text = element_text(size = 15), axis.text.x = element_blank(), axis.title.y = element_text(face = "italic", size = 20), axis.title.x = element_text(size = 20), axis.ticks.x = element_blank()) +
-  scale_fill_manual("", breaks = od1$guild, values = c("chocolate1", "gray", "cadetblue1", "lavender", "greenyellow", "forestgreen", "purple", "tan"), labels = c("soil saprotroph(N=493)", "parasite(N=459)", "wood saprotroph(N=482)", "epiphyte(N=482)", "plant pathogen(N=482)", "litter saprotroph(N=493)", "ECM(N=493)", "ACM(N=482)")) +
+  scale_fill_manual("", breaks = od1$guild, values = c("chocolate1", "gray", "cadetblue1", "greenyellow", "forestgreen", "purple","lavender", "tan"), labels = c("soil saprotroph(N=493)", "parasite(N=459)", "wood saprotroph(N=482)", "plant pathogen(N=482)", "litter saprotroph(N=493)", "ECM(N=493)","epiphyte(N=482)",  "ACM(N=482)")) +
   annotate(x = 1, y = 1.2, "text", label = "g", size = 6) +
   annotate(x = 2, y = 1.5, "text", label = "f", size = 6) +
   annotate(x = 3, y = 1.56, "text", label = "e", size = 6) +
   annotate(x = 4, y = 1.53, "text", label = "e", size = 6) +
-  annotate(x = 5, y = 1.52, "text", label = "de", size = 6) +
+  annotate(x = 5, y = 1.45, "text", label = "d", size = 6) +
   annotate(x = 6, y = 1.4, "text", label = "c", size = 6) +
-  annotate(x = 7, y = 1.35, "text", label = "b", size = 6) +
+  annotate(x = 7, y = 1.705, "text", label = "b", size = 6) +
   annotate(x = 8, y = 1.8, "text", label = "a", size = 6) +
   ylim(0, 3)
+ 
 5. # arrange the two plots
 
-plot_grid(a, b, ncol = 1, labels = c("(a)", "(b)"))
+plot_grid(aa, bb, ncol = 1, labels = c("(a)", "(b)"))
