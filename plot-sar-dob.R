cb78e3eb79a25b5f3620bd540682921e1301d275
wenqisysu <wenqil@umich.edu>
2024-04-09 13:43:12 -0400

    plot level sar for the dob data

---
+++ plot-sar-dob.R
@@ -0,0 +1,132 @@
+## determine the plot-level SAR for the dob sites
+
+d=sample_data(rare_all)
+# select the NEON SITE
+table(d$Project)# the first 908 rows are dob sites with the remaining 5470 being NEON sites
+d1=data.frame(d[,c("geneticSampleID","Site")])
+plotID=substr(d1$geneticSampleID,1,8)
+d1=cbind(d1,plotID)
+iddob=d1$Site[1:908]#a site correspondes to a plot
+idneon=d1$plotID[909:6378]# an unique plotID corresponds to a plot
+plotIDM=data.frame(c(iddob,idneon))
+names(plotIDM)="plotIDM"# the plot id used for the SAR
+row.names(plotIDM)=row.names(d)
+plotIDM=sample_data(plotIDM)
+d<- merge_phyloseq(rare_all, plotIDM)# merge the new plotid with the initial data 
+# select an unique plot and build a SAR within the plot
+
+dob= subset_samples(d,Project=="DoB")# the unique plotID, we have 476 plots
+
+data_sample <- sample_data(dob)%>%data.frame()
+
+dob_plot=unique(data_sample$plotIDM)
+
+d3=subset_samples(dob,plotIDM=="MT1")%>%sample_data()%>%data.frame()
+
+1.# to assin a code to each soil core based on their locations within the 40 by 40 plots
+
+sample_ID=data_sample$geneticSampleID
+
+assign_ID=numeric()
+for (i in 1:908)
+  {
+  sub_sample=sample_ID[i]
+  
+  if(str_detect(sub_sample,"00"))
+    {
+    assign_ID[i]=0
+  }
+  else if(str_detect(sub_sample,"5"))
+  {
+    assign_ID[i]=5
+  }
+  else if(str_detect(sub_sample,"10"))
+  {
+    assign_ID[i]=10
+  }
+  else if(str_detect(sub_sample,"20"))
+  {
+    assign_ID[i]=20
+  }
+  else 
+  {
+    assign_ID[i]=40
+  }
+  }
+
+# add the soil core ID to the phyloseq object
+
+assign_ID=data.frame(assign_ID)
+
+row.names(assign_ID)=row.names(sample_data(dob))
+assign_ID=sample_data(assign_ID)
+
+dob=merge_phyloseq(dob,assign_ID)
+
+2 # get the richness of different spatial scales of core, 5x5, 10x10,20x20 and 40 x40
+
+richness_dob=matrix(nrow=length(dob_plot),ncol=5)
+# the fifth core do not have data for the 0.00 code
+for (i in c(1:4,6:43))
+{
+  cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
+    d0=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID==0)# get the first soil core 
+    d5=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID%in%(c("0","5")))# get the first soil core 
+    d10=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID%in%(c("0","5","10")))# get the first soil core 
+    d20=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID%in%(c("0","5","10","20")))# get the first soil core 
+    d40=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID%in%(c("0","5","10","20","40")))# get the first soil core 
+    richness_dob[i,1]=table(taxa_sums(d0)>0)["TRUE"]%>%as.numeric() 
+    richness_dob[i,2]=table(taxa_sums(d5)>0)["TRUE"]%>%as.numeric() 
+    richness_dob[i,3]=table(taxa_sums(d10)>0)["TRUE"]%>%as.numeric() 
+    richness_dob[i,4]=table(taxa_sums(d20)>0)["TRUE"]%>%as.numeric() 
+    richness_dob[i,5]=table(taxa_sums(d40)>0)["TRUE"]%>%as.numeric() 
+  }
+  
+# when i =5, there is no data for the 0.00 core, so we get the mean value of all the cores for the core
+{
+  d0=subset_samples(dob,plotIDM==dob_plot[i])
+  d0=estimate_richness(d0,measures = "Observed")
+  
+  d5=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID==5)# get the first soil core 
+  
+  d10=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID%in%(c("5","10")))# get the first soil core 
+  d20=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID%in%(c("5","10","20")))# get the first soil core 
+  d40=subset_samples(dob,plotIDM==dob_plot[i]&assign_ID%in%(c("5","10","20","40")))# get the first soil core 
+  
+  richness_dob[i,1]=mean(d0$Observed)
+  richness_dob[i,2]=table(taxa_sums(d5)>0)["TRUE"]%>%as.numeric() 
+  richness_dob[i,3]=table(taxa_sums(d10)>0)["TRUE"]%>%as.numeric() 
+  richness_dob[i,4]=table(taxa_sums(d20)>0)["TRUE"]%>%as.numeric() 
+  richness_dob[i,5]=table(taxa_sums(d40)>0)["TRUE"]%>%as.numeric() 
+}
+
+richness_dob=cbind(dob_plot,richness_dob)%>%data.frame()%>%melt()
+3.# 
+richness_dob[,2:6]=lapply(richness_dob[,2:6], as.numeric)
+
+richness_dob=melt(richness_dob)
+
+area=rep(c(0.0036,25,100,400,1600),each=43)
+
+richness_dob=cbind(richness_dob,area)
+
+# to see the relatiship between area and richness
+
+
+for (i in 1:length(dob_plot))
+{
+  data_subb <- subset(richness_dob,dob_plot==dob_plot [i])
+  
+  fit <-sar_loga(data_subb[,4:3] )
+  plot=plot(fit)
+  
+  # Print the plot
+  print(plot)
+  
+}
+
+
+
+
+ggplot()+geom_point(data=dk,aes(x=lon,y=lat),size=1)+
+  geom_text(data=dk,aes(x=lon,y=lat),size=3,label=substr(dk$geneticSampleID,1,7))
