7b0303c011fa021a1a8446341a914d0cd37ecad9
wenqisysu <wenqil@umich.edu>
2024-05-06 14:47:18 -0400

    core-level richness model

--- plot-level-diversity/core-richness-model.R
+++ plot-level-diversity/core-richness-model.R
@@ -68,7 +68,7 @@ v.glm<-variogram(residuals.glm~ 1, data = train_core_rich)
 
 plot(v.glm)
 
-m.glm<-vgm(1000,"Sph",11,2200)# based on estimation of the semivariance
+m.glm<-vgm(1200,"Exp",11,3000)# based on estimation of the semivariance
 
 m.f.glm<-fit.variogram(v.glm, m.glm)
 m.f.glm
@@ -99,7 +99,7 @@ SK.GLM_core<-krige(residuals.glm~ 1,
 
 
 newloca$SK.GLM_core<-SK.GLM_core$var1.pred
-# Add RF predicted + SK preedicted residuals
+# Add RF predicted + SK predicted residuals
 
 newloca$RK.GLM.bc<-(newloca$GLM_core+newloca$SK.GLM_core)
 
@@ -118,7 +118,7 @@ pred_core=cbind(coordinates_df,attributes_df)
 names(pred_core)[3]="rich"
 
 
-ggplot(pred_core) +
+p1=ggplot(pred_core) +
   geom_point(data=pred_core,pch=15,aes(x=lon, y=lat,color=rich), size=0.275)+
   scale_color_gradient(expression(Richness),low = "blue", high = "yellow")+
   theme(legend.position =c(0.2,0.35), 
@@ -139,4 +139,35 @@ ggplot(pred_core) +
   xlim(-175,-42)+
   geom_point(data=plot_diversity,pch=21,aes(x=lon,y=lat,fill=project),alpha=1)+
   scale_fill_manual("Project",breaks=c("dob","neon"),label=c("DoB","NEON"),values=c("red","seagreen"))
+
+# if we used poss-ion regression
+# the effects of individual variables on the richness
+k=summary(GLM_core)
+
+effect_core=k$coefficients[2:11,]%>%data.frame()
+
+p11=ggplot()+
+  
+  geom_segment(data=effect_core,aes(x=Estimate/10-1.96*Std..Error/10 ,y=1:10,yend=1:10,xend=Estimate/10+1.96*Std..Error/10),color="gray")+
+  geom_point(data=effect_core,aes(x=Estimate/10,y=1:10),color="blue",size=4)+
+  geom_vline(xintercept = 0,linetype="dashed")+
+  theme(legend.position =c(0.2,0.35), 
+        legend.key.size = unit(0.15, "inches"),
+        guides(color = guide_legend(nrow = 2, byrow = TRUE)),
+        legend.title = element_text(size=8),
+        text = element_text(size = 18), 
+        legend.text = element_text(size=8),
+        plot.title = element_text(size = 15, hjust = 0.5), 
+        axis.text.y = element_text(hjust = 0), 
+        axis.title.y = element_text(size = 18), 
+        axis.title.x = element_text(size = 18),
+        axis.ticks.x = element_blank(), 
+        panel.background = element_rect(fill = "NA"), 
+        panel.border = element_rect(color = "black", size = 1.5, fill = NA))+
+  xlab("Effect size")+
+  ylab("")+
+  scale_y_continuous(breaks = 1:10, label = c("SoilOC", "pH", "SoilN", "Sand", "MAT", "MDR", "Tem.seas.","MAP","Pre.seas.","PWQ")) +
+  xlab("Effect size and 95% CI") +
+  ggtitle("Core-level richness (33.3%)") 
+  
 
