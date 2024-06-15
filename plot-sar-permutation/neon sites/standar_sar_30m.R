0a3c948cf8b938bc56deb0edc1a38239fba54313
wenqisysu <wenqil@umich.edu>
2024-06-14 22:55:33 -0400

    estimated the z value for the neon site

--- plot-sar-permutation/neon sites/standar_sar_30m.R
+++ plot-sar-permutation/neon sites/standar_sar_30m.R
@@ -1,11 +1,9 @@
 ## to determine the richness at the 30 by 30 square
 
-
-
 ##
 ## select the first point and to see how many cores are selected within
 
-## the oring of the square
+## the orig of the square
 pp=sample_data(rare_all_assign)%>%data.frame()
 pp=unique(pp$plotIDM)
 ori=expand.grid(x=seq(0,10,0.5),y=seq(0,10,0.5))
@@ -39,7 +37,7 @@ ggplot()+
   geom_vline(xintercept = 4.5)+
   geom_hline(yintercept =4.5 )
 
-## for each plot, we can selet the origin that lead to more number of cores included
+## for each plot, we can select the origin that lead to more number of cores included
   
   richness_plot=numeric()
   for(k in 1:length(pp))
@@ -68,4 +66,50 @@ ggplot()+
   richness_plot[k]=richness%>%mean(na.rm = TRUE)
   }
   
+  
+  ## get the richness
+  
+  richness_plot%>%data.frame()%>%mutate(plotid=pp)
+  
+  
+  load("~/soil-sar/richness_subplot10_neon_standar.RData")
+  load("~/soil-sar/richness_subplot20_neon_standar.RData")
+  load("~/soil-sar/plot-sar-permutation/richness_subplot40_mean_neon_standar.RData") 
+  
+# cbind all the data sets
+  
+  richness_subplot10_neon_standar%>%mutate(area=rep(100,length.out=n))->richness_subplot10_neon_standar
+  
+  richness_subplot20_neon_standar%>%mutate(area=rep(400,length.out=n))->richness_subplot20_neon_standar
+  
+  richness_subplot30_neon_standar=richness_plot%>%data.frame()%>%mutate(plotid=pp)%>%mutate(area=rep(900,length.out=n))%>%rename_all(~paste0(c("mean_value","plotid","area")))%>%select(plotid,mean_value,area)
+  
+  richness_subplot40_neon_standar=richness_subplot40_mean_neon_standar%>%rename_all(~paste0(c("mean_value","plotid","area")))%>%select(plotid,mean_value,area)
+  
+  ## cbind the data across the scales
+  
+  richness_data_neon=bind_rows(richness_subplot10_neon_standar%>%select(plotid, mean_value, area),richness_subplot20_neon_standar%>%select(plotid, mean_value, area),richness_subplot30_neon_standar%>%select(plotid, mean_value, area),richness_subplot40_neon_standar%>%select(plotid, mean_value, area))%>%filter(!is.na(mean_value ))
+
+  richness_data_neon%>%count(plotid)%>%filter(n>2)->richness_data_neon_sar# we have 156 plots
+  
+  save(richness_data_neon,file="richness_data_neon.RData")
+  
+  # for the estimation of the z and  c value
+  
+ 
+  a=unique(richness_data_neon_sar$plotid)
+  para_estimate=data.frame(ncol=3,nrow=length(a))
+  for (i in 1:length(a))
+    {
+    mod=lm(formula = log(mean_value) ~log(area), data =richness_data_neon%>%filter(plotid==a[i])) 
+    para_estimate[i,1]=a[i]
+    para_estimate[i,2]=coef(mod)[1]
+    para_estimate[i,3]=coef(mod)[2]
+    
+  }
+  para_estimate= para_estimate%>%rename_all(~paste0(c("plotID","logc","z")))
+  
+  richness_data_neon_sar%>%rename(plotID=plotid)%>%left_join(para_estimate,by="plotID")->tem
+ 
+   ## good to thing that number of points does not affect the estimated z
   
