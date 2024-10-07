0202cbdadc7598d32be819dcd499f86b654ac7a4
wenqisysu <wenqil@umich.edu>
2024-10-07 17:34:20 -0400

    crop the boreal regions

--- plot-sar-permutation/land use effect/figure3-cropped.R
+++ plot-sar-permutation/land use effect/figure3-cropped.R
@@ -49,47 +49,6 @@ r <- rasterize(biomes, r, field = "LABEL")  # 'field' can be a column name or a 
 
 coords_present%>%data.frame()%>%rename_all(~paste0(c("lon","lat")))->coords_present
 
-land_use_data=matrix(ncol=33,nrow=dim(coords_present)[1])
-for (i in 1:33)
-{
-  cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
-  
-  r_projected <- terra::project(new_raster[[i]], "EPSG:5070")
-  # transformed the coordinates
-  
-  #points <- vect(coords_present%>%data.frame()%>%rename_all(~paste0(c("lon","lat")), crs=crs(r_projected )))
-  points_sf <- st_as_sf(coords_present, coords = c("lon", "lat"), crs = 4326) # CRS 4326 is WGS84
-  coordinates_equal_area <- st_transform(points_sf, "EPSG:5070")
-  extracted_values <- terra::extract(r_projected, coordinates_equal_area)%>%as.matrix()
-  land_use_data[,i]=extracted_values[,2]
-}
-
-###
-
-# get the coverage of natural and human-dominated land use type for each grid
-
-PFT_2015=coords_present%>%bind_cols(land_use_data%>%data.frame()%>%rename_all(~paste0(names(raster1))))
-
-PFT_2015=PFT_2015%>%mutate(tree=PFT1+PFT2+PFT3+PFT4+PFT5+PFT6+PFT7+PFT8,shrup=PFT9+PFT10+PFT11,grass=PFT12+PFT13+PFT14,
-                           crop=PFT15+PFT16+PFT17+PFT18+PFT19+PFT20+PFT21+PFT22+PFT23+PFT24+PFT25+PFT26+PFT27+PFT28+PFT29+PFT30)
-
-temp=PFT_2015[,c("PFT1","PFT2","PFT3","PFT4","PFT5","PFT6","PFT7","PFT8","PFT9","PFT10","PFT11","PFT12","PFT13","PFT14")]
-
-No_crop=apply(temp, 1, sum)%>%data.frame()
-names(No_crop)="No_crop"
-
-PFT_2015%>%bind_cols(No_crop)%>%mutate(area=11637.87^2)->PFT_2015
-
-# the area is fixed for each grid 11637.87 m^2 at the resolution of 10 min of a degree
-
-
-#get the richness within each grid based on the SAR
-#S=cA^z
-#get the biomes for each gird
-
-grid_level_biomes=terra::extract(r,PFT_2015[,c("lon","lat")])
-
-PFT_2015%>%bind_cols(biomes=grid_level_biomes%>%dplyr::select(LABEL))->PFT_2015
 
 # based on the SAR parameters to determine the total fungal richness for each grid
 
@@ -105,26 +64,30 @@ full_guild_affinity_data=readRDS("full_guild_affinity_data.rds")
 full_guild_affinity_data%>%rename(LABEL=biome)->full_guild_affinity_data
 
 
-richness_2015=matrix(ncol=9,nrow=dim(PFT_2015)[1])
-for (i in 1:9)
-{
-  richness_temp=PFT_2015%>%dplyr::select(crop,No_crop,LABEL,area)%>%
-    left_join(full_guild_affinity_data%>%dplyr::select(guild, mean_zvalue, mean_cvalue,LABEL,affinity)%>%
-                filter(guild==guild_type[i]),by="LABEL")%>%
-    
-    mutate(richness=mean_cvalue*(affinity*crop/100*area+No_crop/100*area)^mean_zvalue)%>%as.matrix()
-  richness_2015[,i]=richness_temp[,"richness"]
-}
 
-richness_2015%>%data.frame()%>%mutate(across(everything(), as.numeric))->richness_2015
+# for the scenario of rcp585 for land use change effect
 
+bbox <- st_bbox(c(xmin = -2665004, ymin = 2157670 , xmax = 3188701, ymax = 5400000 ), crs = st_crs(canadian_projected))
+bbox_sf <- st_as_sfc(bbox)
+cropped_province <- st_crop(canadian_projected, bbox_sf)
+canada_clipped=cropped_province
+bbox <- st_bbox(sf_object)#to see the range of the sf object
 
+# the effect of land use change in the scenario of rcp585
+
+raster1 <- rast("GCAM_Demeter_LU_ssp2_rcp45_hadgem_2015.nc")# use the 2015 data as the baseline
 raster2 <- rast("GCAM_Demeter_LU_ssp2_rcp45_hadgem_2100.nc")# use the 2015 data as the baseline
 
-ext(raster2) <- c(-90, 90, -180, 180)
-crs(raster2) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+raster3 <- rast("GCAM_Demeter_LU_ssp5_rcp85_hadgem_2100.nc")# use the 2015 data as the baseline
+
+raster4 <- rast("GCAM_Demeter_LU_ssp5_rcp85_hadgem_2015.nc")# use the 2015 data as the baseline
+
+my_function_raster=function(data)
+{
+ext(data) <- c(-90, 90, -180, 180)
+crs(data) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 b <- as(extent (-72, -18, -170, -55), "SpatialPolygons")
-coarser_raster <- aggregate(raster2, fact = 3, fun = mean) # convert it to a coarse resolution
+coarser_raster <- aggregate(data, fact = 3, fun = mean) # convert it to a coarse resolution
 cropped_raster<- crop(coarser_raster, b)
 cropped_raster=flip(t(cropped_raster),direction = "horizontal")# transpose the raster to a right position
 new_raster =list()
@@ -136,92 +99,121 @@ for (i in 1:33){
   values(temp) <- values(cropped_raster[[i]]) 
   new_raster[[i]]=temp#each returns a transformed raster
 }
-
-# make equal-area projection for each raster and extract the land-use cover data
-
-land_use_data_2100=matrix(ncol=33,nrow=dim(coords_present)[1])
+# make equal area projection for each raster and extract the values
+df_temp=matrix(ncol=33,nrow=dim(coords_present)[1])
 for (i in 1:33)
 {
   cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
-  
   r_projected <- terra::project(new_raster[[i]], "EPSG:5070")
   # transformed the coordinates
-  
   #points <- vect(coords_present%>%data.frame()%>%rename_all(~paste0(c("lon","lat")), crs=crs(r_projected )))
   points_sf <- st_as_sf(coords_present, coords = c("lon", "lat"), crs = 4326) # CRS 4326 is WGS84
   coordinates_equal_area <- st_transform(points_sf, "EPSG:5070")
   extracted_values <- terra::extract(r_projected, coordinates_equal_area)%>%as.matrix()
-  land_use_data_2100[,i]=extracted_values[,2]
+  df_temp[,i]=extracted_values[,2]
 }
-
-# check if the total land use cover approximates 100%
 #apply(land_use_data,1,sum)%>%data.frame()%>%rename_all(~paste0("cover"))->oop
-
-
-
-PFT_2100=coords_present%>%bind_cols(land_use_data_2100%>%data.frame()%>%rename_all(~paste0(names(raster2))))
-
-PFT_2100=PFT_2100%>%mutate(tree=PFT1+PFT2+PFT3+PFT4+PFT5+PFT6+PFT7+PFT8,shrup=PFT9+PFT10+PFT11,grass=PFT12+PFT13+PFT14,
-                           crop=PFT15+PFT16+PFT17+PFT18+PFT19+PFT20+PFT21+PFT22+PFT23+PFT24+PFT25+PFT26+PFT27+PFT28+PFT29+PFT30)
-
-temp=PFT_2100[,c("PFT1","PFT2","PFT3","PFT4","PFT5","PFT6","PFT7","PFT8","PFT9","PFT10","PFT11","PFT12","PFT13","PFT14")]
-
+df_temp1=coords_present%>%bind_cols(df_temp%>%data.frame()%>%rename_all(~paste0(names(data))))
+df_temp1=df_temp1%>%mutate(tree=PFT1+PFT2+PFT3+PFT4+PFT5+PFT6+PFT7+PFT8,shrup=PFT9+PFT10+PFT11,
+                           grass=PFT12+PFT13+PFT14,
+                      crop=PFT15+PFT16+PFT17+PFT18+PFT19+PFT20+PFT21+PFT22+PFT23+PFT24+PFT25+PFT26+PFT27+PFT28+PFT29+PFT30)
+temp=df_temp1[,c("PFT1","PFT2","PFT3","PFT4","PFT5","PFT6","PFT7","PFT8","PFT9","PFT10","PFT11","PFT12","PFT13","PFT14")]
 No_crop=apply(temp, 1, sum)%>%data.frame()
 names(No_crop)="No_crop"
+df_temp1%>%bind_cols(No_crop)%>%mutate(area=11637.87^2)->df_temp1
 
-PFT_2100%>%bind_cols(No_crop)%>%mutate(area=11637.87^2)->PFT_2100
-
-# the area is fixed as 11637.87^2 for each grid at the resolution of 10 min
-
-
+# the area is fixed 11637.87^2 with a resolution of 10 min
 #get the richness within each grid
 #s=cA^z
-# assign the biome type for each gird
-
-grid_level_biomes=terra::extract(r,PFT_2100[,c("lon","lat")])
-
-PFT_2100%>%bind_cols(biomes=grid_level_biomes%>%dplyr::select(LABEL))->PFT_2100
-
-
-
-
-richness_2100=matrix(ncol=9,nrow=dim(PFT_2100)[1])
+# get the biomes for each gird
+grid_level_biomes=terra::extract(r,df_temp1[,c("lon","lat")])
+df_temp1%>%bind_cols(biomes=grid_level_biomes%>%dplyr::select(LABEL))->df_temp1
+# get the richness for each guild and each grid
+guild_type=c("AM","EM","soilsap","littersap","woodsap","plapat","para","epiphy","all")
+richness_derived=matrix(ncol=9,nrow=dim(df_temp1)[1])
 for (i in 1:9)
 {
-  richness_temp=PFT_2100%>%dplyr::select(crop,No_crop,LABEL,area)%>%
+  richness_temp=df_temp1%>%dplyr::select(crop,No_crop,LABEL,area)%>%
     left_join(full_guild_affinity_data%>%dplyr::select(guild, mean_zvalue, mean_cvalue,LABEL,affinity)%>%
                 filter(guild==guild_type[i]),by="LABEL")%>%
-    
     mutate(richness=mean_cvalue*(affinity*crop/100*area+No_crop/100*area)^mean_zvalue)%>%as.matrix()
-  richness_2100[,i]=richness_temp[,"richness"]
+  richness_derived[,i]=richness_temp[,"richness"]
+}
+richness_derived=richness_derived%>%data.frame%>%mutate(across(everything(), ~ as.numeric(as.character(.))))
+#colnames(richness_derived)=c("AM","EM","soilsap","littersap","woodsap","plapat","para","epiphy","all")
+#richness_derived=melt(richness_derived)
+return(richness_derived)
 }
 
-richness_2100%>%data.frame()%>%mutate(across(everything(), as.numeric))->richness_2100
+# get the richness data for different scenarios
+
+richness_2015=my_function_raster(raster1)
+richness_2010_rcp245=my_function_raster(raster2)
+richness_2010_rcp585=my_function_raster(raster3)
+
+
+# difference in the richness among the two time points
+
+
+colnames(richness_2100_rcp585)=c("AM","EM","soilsap","littersap","woodsap","plapat","para","epiphy","all")
+
+colnames(richness_2015)=c("AM","EM","soilsap","littersap","woodsap","plapat","para","epiphy","all")
+
 
 species_change_land_rcp245=(richness_2100-richness_2015)/richness_2015
-
 colnames(species_change_land_rcp245)=guild_type
-
 species_change_land_rcp245=melt(species_change_land_rcp245)
 
-species_change_land_rcp245=bind_cols(species_change_land_rcp245,coords_present)
 
-richness_2015=bind_cols(richness_2015,coords_present)
+species_change_land_rcp585=(richness_2100_rcp585-richness_2015)/richness_2015
+species_change_land_rcp585%>%melt()->species_change_land_rcp585
 
-# for the scenario of rcp585 for land use change effect
 
-bbox <- st_bbox(c(xmin = -2665004, ymin = 2157670 , xmax = 3188701, ymax = 5400000 ), crs = st_crs(canadian_projected))
-bbox_sf <- st_as_sfc(bbox)
-cropped_province <- st_crop(canadian_projected, bbox_sf)
-canada_clipped=cropped_province
-bbox <- st_bbox(sf_object)#to see the range of the sf object
+species_change_land_rcp585%>%filter(variable=="all")%>%
+  bind_cols(coords_present) ->change_richness_rcp585
+
+
 
 
 # project the results
+# the function to project the data based on a data.frame
+
+my_function_project=function(data)
+{
+  if ("x"%in%colnames(data))
+  {
+    points <- vect(data, geom = c("x", "y"), crs = "EPSG:4326")  # Assuming WGS84 coordinates
+    raster_template <- rast(ext(points), resolution = 0.17, crs = "EPSG:4326")  # Resolution of 1 degree
+    raster <- rasterize(points, raster_template, field = "value")
+    }
+  else{
+    points <- vect(data, geom = c("lon", "lat"), crs = "EPSG:4326")  # Assuming WGS84 coordinates
+    raster_template <- rast(ext(points), resolution = 0.17, crs = "EPSG:4326")  # Resolution of 1 degree
+    
+    raster <- rasterize(points, raster_template, field = "group")
+    }
+   target_crs <- "EPSG:5070"
+  raster_equal_area <- project(raster, target_crs,method="near")# there are options for the method used
+  raster_df <- as.data.frame(raster_equal_area, xy = TRUE,)
+  return(raster_df )
+}
+
+###
+
+#for the latitude trend
+
+my_function_latitude_patterns_crop=function(data)
+{
+  data%>%rename(lon=x,lat=y)->temp_data
+  latitude_patterns <- temp_data%>% group_by(lat) %>%
+    summarise(mean_value = mean(value,na.rm=TRUE),sd_value = sd(value,na.rm=TRUE))
+  return(latitude_patterns )
+}
+
 
 ###############the effect of land use change######################
 
-# the spatial patterns 
+# the spatial patterns for the climate change impact
 species_change_land_rcp245_all=species_change_land_rcp245%>%
   filter(variable=="all")%>%bind_cols(coords_present)%>%rename(x=lon,y=lat)
 
@@ -233,18 +225,16 @@ species_change_land_rcp585_all=my_function_project(species_change_land_rcp585_al
 
 
 
+
+
 # get the latitude pattern
 summary_data=my_function_latitude_patterns(species_change_land_rcp245%>%filter(variable=="all"))
-
 summary_data_land_rcp585=my_function_latitude_patterns(species_change_land_rcp585%>%filter(variable=="all"))
 
 # get the overall effect of different guilds
 
 effect_land_overall_245=my_function_overall_effect(species_change_land_rcp245)
 effect_land_overall_585=my_function_overall_effect(species_change_land_rcp585)
-
-
-
 
 
 
@@ -259,27 +249,11 @@ species_change_climate_rcp585=readRDS("species_change_climate_rcp585.rds")
 #defin the region to be retained 
 
 land_effect=bind_cols(coords_present,species_change_land_rcp245%>%filter(variable=="all"))
-
 crs(r_climate) <- CRS("+proj=longlat +datum=WGS84")
-
 r_land <- rasterFromXYZ(land_effect[, c("lon", "lat", "value")])
-
 r_tem=as.data.frame(r_land, xy = TRUE, na.rm = TRUE)
-
 # see the overall effects of climate change
 #need to maks the data and then bind different guilds
-
-crop_data_rcp585_climate=list()
-for (i in 1:9)
-{
-  climate_effect=bind_cols(coords_present,species_change_climate_rcp585%>%filter(variable==guild_type[i]))
-  r_climate <- rasterFromXYZ(climate_effect[, c("lon", "lat", "value")])
-  crs(r_climate) <- CRS("+proj=longlat +datum=WGS84")
-  masked_raster <- mask(r_climate, r_land)
-  raster_df <- as.data.frame(masked_raster, xy = TRUE, na.rm = TRUE)
-  colnames(raster_df) <- c("x", "y", "value")
-  crop_data_rcp585_climate[[i]]=raster_df%>%mutate(variable=rep(guild_type[i],dim(raster_df)[1]))
-}
 
 
 crop_data=list()
@@ -294,6 +268,19 @@ for (i in 1:9)
   crop_data[[i]]=raster_df%>%mutate(variable=rep(guild_type[i],40650))
 }
 
+crop_data_rcp585_climate=list()
+for (i in 1:9)
+{
+  climate_effect=bind_cols(coords_present,species_change_climate_rcp585%>%filter(variable==guild_type[i]))
+  r_climate <- rasterFromXYZ(climate_effect[, c("lon", "lat", "value")])
+  crs(r_climate) <- CRS("+proj=longlat +datum=WGS84")
+  masked_raster <- mask(r_climate, r_land)
+  raster_df <- as.data.frame(masked_raster, xy = TRUE, na.rm = TRUE)
+  colnames(raster_df) <- c("x", "y", "value")
+  crop_data_rcp585_climate[[i]]=raster_df%>%mutate(variable=rep(guild_type[i],dim(raster_df)[1]))
+}
+
+
 #to bind all the data for climate effect
 combined_data=bind_rows(crop_data[[1]],crop_data[[2]],crop_data[[3]],
                         crop_data[[4]],crop_data[[5]],crop_data[[6]],
@@ -305,41 +292,25 @@ combined_data_climate_rcp585=bind_rows(crop_data_rcp585_climate[[1]],crop_data_r
 
 
 
-#for the latitude trend
 
-my_function_latitude_patterns_crop=function(data)
-{
-  data%>%rename(lon=x,lat=y)->temp_data
-  latitude_patterns <- temp_data%>% group_by(lat) %>%
-    summarise(mean_value = mean(value,na.rm=TRUE),sd_value = sd(value,na.rm=TRUE))
-  return(latitude_patterns )
-}
-
-#for the climate change impact on overal fungal diversity
-
+#for the climate change impact on overall fungal diversity
 # for the latitude patterns for climate change impacts
 
-
-
-climate_induced_change_richness_rcp245=my_function_project(crop_data[[9]])
-
+climate_induced_change_richness_rcp245=my_function_project(crop_data[[9]])#after cropping
 climate_induced_change_richness_rcp585=my_function_project(crop_data_rcp585_climate[[9]])
 
 
 summary_data_climate_rcp245=my_function_latitude_patterns_crop(crop_data[[9]])
-
 summary_data_climate_rcp585=my_function_latitude_patterns_crop(crop_data_rcp585_climate[[9]])
 
 # for the overall effect for climate change
 climate_245_overall_effect=my_function_overall_effect(combined_data)
-
 climate_245_overall_effect$variable=factor(climate_245_overall_effect$variable,
                                            levels=c("AM","EM","soilsap","littersap" ,
                                                     "woodsap","plapat" ,"para" ,"epiphy","all"))
 
 
 climate_585_overall_effect=my_function_overall_effect(combined_data_climate_rcp585)
-
 climate_585_overall_effect$variable=factor(climate_585_overall_effect$variable,
                                            levels=c("AM","EM","soilsap","littersap" ,
                                                     "woodsap","plapat" ,"para" ,"epiphy","all"))
@@ -347,21 +318,7 @@ climate_585_overall_effect$variable=factor(climate_585_overall_effect$variable,
 
 
 
-effect_land_overall_585=my_function_overall_effect(species_change_land_rcp585)
-effect_land_overall_585$variable=factor(effect_land_overall_585$variable,levels=guild_type)
 
-
-# the effect of land use change on the overall fungal diversity
-
-species_change_land_rcp585%>%filter(variable=="all")->species_change_land_rcp585_all
-species_change_land_rcp585_all%>%bind_cols(coords_present)%>%rename(x=lon,y=lat)->temp
-change_richness_rcp585=my_function_project(temp)
-
-species_change_land_rcp585_all=my_function_project(species_change_land_rcp585_all)
-
-summary_data_land_rcp585=my_function_latitude_patterns(species_change_land_rcp585_all)
-
-# for the climate change impact
 
 
 #create the plots
@@ -457,13 +414,9 @@ p_land_overall_245=ggplotGrob(ggplot(data=effect_land_overall_245,aes(fill=type,
 #aes(x=rep(c(0.25020640385390, -0.2011087320765014266, -0.1312420753516064,  0.1287910320636994127, -0.086540611321197594057,  0.1027649490386, -0.128586038102421426806,
 #-0.13132754493784, -0.10245710314108882),each=2),y=variable),label="***"))
 
-
-#
-
-
 p_climate_245=ggplotGrob(ggplot(climate_induced_change_richness_rcp245) +
                            geom_point(data = climate_induced_change_richness_rcp245, pch=15,aes(x = x, y = y, color = last*100), size = 0.175) +
-                           scale_color_gradient2(expression("Change %"), high = "#1173EE", mid="white",low = "#ee8c11", na.value = "white",limits=c(-50,50))+
+                           scale_color_gradient2(expression("Change %"), high = "#1173EE", mid="white",low = "#ee8c11", na.value = "white")+
                            geom_sf(data = us_projected, fill = NA, size=0.01,color = "black")+
                            geom_sf(data = cropped_province, fill = NA, size=0.01,color = "black")+
                            geom_sf(data = mexico_projected, fill = NA, size=0.01,color = "black")+
@@ -560,8 +513,8 @@ p_climate_overall_245=ggplotGrob(ggplot(data=climate_245_overall_effect,aes(fill
 
 
 
-p_land_585=ggplotGrob(ggplot(change_richness_rcp585) +
-                        geom_point(data = change_richness_rcp585, pch=15,aes(x = x, y = y, color = last*100), size = 0.175) +
+p_land_585=ggplotGrob(ggplot(species_change_land_rcp585_all) +
+                        geom_point(data = species_change_land_rcp585_all, pch=15,aes(x = x, y = y, color = last*100), size = 0.175) +
                         scale_color_gradient2(expression("Change %"), high = "#1173EE", mid="white",low = "#ee8c11", na.value = "white")+
                         geom_sf(data = us_projected, fill = NA, size=0.01,color = "black")+
                         geom_sf(data = canada_clipped, fill=NA,size=0.01,color = "black")+
