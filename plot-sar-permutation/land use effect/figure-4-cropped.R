f0ca6f537c3508a1f65ddf450718dc445daec458
wenqisysu <wenqil@umich.edu>
2024-11-26 19:57:09 -0500

    figure4

--- plot-sar-permutation/land use effect/figure-4-cropped.R
+++ plot-sar-permutation/land use effect/figure-4-cropped.R
@@ -83,8 +83,9 @@ my_function_guild_loss=function(data){
                            joint_effect%in%c("L-loss-high _ C-loss-low","L-loss-high _ NA","L-loss-high _ C-gain-high","L-loss-high _ C-gain-low")~"L-high-loss", 
                            joint_effect%in%c("L-loss-low _ C-loss-high","NA _ C-loss-high",
                                              "L-gain-high _ C-loss-high","L-gain-low _ C-loss-high")~"C-high-loss",
+                           joint_effect=="NA _ NA"~"NA",
                            TRUE~"other"))->df4
-  df4=my_function(df4)
+  #df4=my_function(df4)
   return(df4)
 }
 
@@ -113,9 +114,10 @@ my_function_guild_gain=function(data){
                            joint_effect=="L-gain-low _ C-gain-low"~"both-low-gain", 
                            joint_effect%in%c("L-gain-high _ C-loss-high","L-gain-high _ C-gain-low","L-gain-high _ C-loss-low","L-gain-high _ NA")~"L-high-gain", 
                            joint_effect%in%c("L-loss-low _ C-gain-high","L-gain-low _ C-gain-high","NA _ C-gain-high"  ,"L-loss-high _ C-gain-high")~"C-high-gain",
+                           joint_effect=="NA _ NA"~"NA",
                            TRUE~"other"))->df4
   
-  df4=my_function(df4)
+  #df4=my_function(df4)
   return(df4)
 }
 
@@ -226,7 +228,7 @@ for (i in 1:9)
     rename(land_effect=value.x,climate_effect=value.y)
 }
 
-common_theme=theme(legend.position = c(0.15,0.5660),
+common_theme=theme(legend.position = c(0.02611,0.4660),
                    legend.margin = margin(t = -5, r = -5, b = -5, l = 0),
                    legend.text = element_text(size=12),
                    legend.title  = element_text(size=12),
@@ -243,6 +245,25 @@ common_theme=theme(legend.position = c(0.15,0.5660),
                    panel.border = element_blank())
 
 
+#crop the region based on the study regions
+
+
+crop_data=list()
+for (i in 1:9)
+{
+
+  df5 <- rasterFromXYZ(df5[, c("x", "y", "group")])
+  crs(df5) <- CRS("+proj=longlat +datum=WGS84")
+  masked_raster <- mask(df5, r_land)
+  
+  raster_df <- as.data.frame(masked_raster, xy = TRUE, na.rm = TRUE)
+  colnames(raster_df) <- c("x", "y", "value")
+  crop_data[[i]]=raster_df%>%mutate(variable=rep(guild_type[i],dim(raster_df)[1]))
+}
+
+
+
+
 
 map_data=c("data_both_effect_rcp245","data_both_effect_rcp585")
 
@@ -255,13 +276,13 @@ for (i in 1:2)
   df5$group=as.factor(df5$group)
   data_percent=table(df5$group)[1:5]/sum(table(df5$group)[1:5])->d
   d%>%data.frame()%>%
-    mutate(group=c("Both high","Both low","Climate high","Land-use high","Other"))->data_percent
+    mutate(group=c("Both high","Both low","Climate high","Land high","Other"))->data_percent
   if(i<2){
     map_loss[[i]]=ggplot()+
       geom_point(data=df5,pch=15,aes(x=x,y=y,color=group),size=0.01)+
       scale_color_manual("",breaks=c("0","1","2","3","4","5"),
-                         labels=c(paste0(data_percent$group ," (",round(data_percent$Freq*100,1),"%)"),""),
-                         values=c("#E92316","#E3A72F",  "#32CD92","#CD326D","gray88","white"))+
+                         labels=c(paste0(data_percent$group ," (",sprintf("%.1f%%", data_percent$Freq*100) ,")"),""),
+                         values=c("#6A4C93","#E3A72F",  "#32CD92","#CD326D","gray88","white"))+
       
       #geom_sf(data=st_as_sf(north_america_cropped),size=0.1, col="black", fill=alpha("white", 0),linetype = "solid")+
       ggtitle("Species loss rate (RCP4.5-SSP2)")+
@@ -286,8 +307,8 @@ for (i in 1:2)
     map_loss[[i]]=ggplot()+
       geom_point(data=df5,pch=15,aes(x=x,y=y,color=group),size=0.01)+
       scale_color_manual("",breaks=c("0","1","2","3","4","5"),
-                         labels=c(paste0(data_percent$group ," (",round(data_percent$Freq*100,1),"%)"),""),
-                         values=c("#E92316","#E3A72F","#32CD92","#CD326D","gray88","white"))+
+                         labels=c(paste0(data_percent$group ," (",sprintf("%.1f%%", data_percent$Freq*100) ,")"),""),
+                         values=c("#6A4C93","#E3A72F","#32CD92","#CD326D","gray88","white"))+
       
       #geom_sf(data=st_as_sf(north_america_cropped),size=0.1, col="black", fill=alpha("white", 0),linetype = "solid")+
       ggtitle("Species loss rate (RCP5.8-SSP5)")+
@@ -325,7 +346,7 @@ for (i in 1:2)
       geom_point(data=df5,pch=15,aes(x=x,y=y,color=group),size=0.01)+
       scale_color_manual("",breaks=c("0","1","2","3","4","5"),
                          labels=c(paste0(data_percent$group ," (",round(data_percent$Freq*100,1),"%)"),""),
-                         values=c("#E92316","#E3A72F","#32CD92","#CD326D","gray88","white"))+
+                         values=c("#6A4C93","#E3A72F","#32CD92","#CD326D","gray88","white"))+
       #geom_sf(data=st_as_sf(north_america_cropped),size=0.1, col="black", fill=alpha("white", 0),linetype = "solid")+
       ggtitle("Species gain rate (RCP4.5-SSP2)")+
       guides(color = guide_legend(override.aes = list(size = 2)))+
@@ -350,7 +371,7 @@ for (i in 1:2)
       
       scale_color_manual("",breaks=c("0","1","2","3","4","5"),
                          labels=c(paste0(data_percent$group ," (",round(data_percent$Freq*100,1),"%)"),""),
-                         values=c("#E92316","#E3A72F","#32CD92","#CD326D","gray88","white"))+
+                         values=c("#6A4C93","#E3A72F","#32CD92","#CD326D","gray88","white"))+
       
       #geom_sf(data=st_as_sf(north_america_cropped),size=0.1, col="black", fill=alpha("white", 0),linetype = "solid")+
       ggtitle("Species gain rate (RCP5.8-SSP5)")+
@@ -394,12 +415,17 @@ my_function_guild_loss_overall=function(data){
   bind_cols(df2%>%dplyr::select(lon,lat,type_C),df1%>%dplyr::select(type_L))->df3 
   
   df3%>%mutate(joint_effect=paste(df3$type_L,"_",df3$type_C))%>%
-    mutate(group=case_when(joint_effect=="L-loss _ C-loss"~"both-loss", 
-                           #joint_effect=="L-gain _ C-gain"~"both-gain", 
-                           joint_effect=="L-NA _ C-NA"~"both-NA", 
-                           joint_effect%in%c("L-loss _ NA",  "L-loss _ C-gain", "L-loss _ C-none")~"L-loss", 
-                           joint_effect%in%c( "L-none _ C-loss", "NA _ C-loss", "L-gain _ C-loss")~"C-loss",
-                           TRUE~"other"))->df4
+    mutate(group=case_when(joint_effect=="L-loss _ C-loss"~"Climate loss.Land loss", 
+                           joint_effect=="L-gain _ C-gain"~"Climate gain.Land gain", 
+                           joint_effect%in%c("L-NA _ C-NA", "L-none _ C-no")~"NA",
+                           joint_effect%in%c("L-loss _ C-NA",  "L-loss _ C-none")~"Climate NA.Land loss", 
+                           joint_effect%in%c( "L-none _ C-loss", "NA _ C-loss" )~"Climate loss.Land NA",
+                           joint_effect%in%c(  "L-gain _ C-NA",  "L-gain _ C-no"  )~"Climate NA.Land gain",
+                           joint_effect=="L-none _ C-gain"~"Climate gain.Land NA",
+                           joint_effect=="L-loss _ C-gain"~"Climate gain.Land loss", 
+                           
+                           joint_effect=="L-gain _ C-loss"~"Climate loss.Land gain"
+                           ))->df4
   #df4=my_function(df4)
   return(df4)
 }
@@ -427,12 +453,22 @@ my_function_guild_gain_overall=function(data){
       joint_effect=="L-NA _ C-NA"~"both-NA", 
       joint_effect%in%c("L-gain _ C-loss", "L-gain _ C-NA", "L-gain _ C-no" )~"L-gain", 
       joint_effect%in%c( "L-none _ C-gain","L-NA _ C-gain","L-loss _ C-gain")~"C-gain",
+      joint_effect=="NA _ NA"~"NA",
       TRUE~"other"))->df4
   
   #df4=my_function(df4)
   return(df4)
 }
+
 ## for species loss rate for the two factors
+
+color_palette <- c(
+  "#E41A1C", "#377EB8", "#4DAF4A", 
+  "#984EA3", "#FF7F00", "#FFFF33", 
+  "#A65628", "#F781BF", "white"
+)
+
+guild_names=c("AM","EM","Soil saprotroph","Litter saprotroph","Wood saprotroph","Plant pathogen","Parasite","Epiphyte","All")
 
 different_guilds=list()
 
@@ -444,21 +480,23 @@ for (i in 1:2)
   data=get(map_data[i])[[m]]    
   df5=my_function_guild_loss_overall(data)  
   df5=my_function_project(df5)
-  df5$group=factor(df5$group,levels=c("0","2","3","4","1"))
+  df5$group=factor(df5$group,levels=c("0","1","2","3","4","5","6","7"))
   
-  data_percent=table(df5$group)[c(1:4)]/sum(table(df5$group)[c(1:4)])->d
+  data_percent=table(df5$group)[c(1:8)]/sum(table(df5$group)[c(1:8)])->d
   d%>%data.frame()%>%
-    mutate(group=c("Both loss","Climate loss","Land loss","Other"))->data_percent
+    mutate(group=c("Climate gain.Land gain","Climate gain.Land loss","Climate gain.Land NA","Climate loss.Land gain", "Climate loss.Land loss","Climate loss.Land NA","Climate NA.Land gain","Climate NA.Land loss"))->data_percent
   if(i<2){
     map_loss_overall[[i]]=ggplot()+
       geom_point(data=df5,aes(x=x,y=y,color=group),pch=15,size=0.01)+
-      scale_color_manual("",breaks=c("0","2","3","4","1"),
-                         labels=c(paste0(data_percent$group ," (",round(data_percent$Freq*100,1),"%)"),""),
-                         values=c("#CD326D","#E3A72F","#32CD92","gray88","white"))+
+      scale_color_manual("",breaks=c("0","1","2","3","4","5","6","7"),
+                         labels=c(paste0(data_percent$group ," (",round(data_percent$Freq*100,1),"%)")),
+                         values=c( "#E41A1C", "#377EB8", "#4DAF4A", 
+                                   "#984EA3", "#FF7F00", "#FFFF33", 
+                                   "#A65628", "#F781BF"))+
       #geom_sf(data=st_as_sf(north_america_cropped),size=0.1, col="black", fill=alpha("white", 0),linetype = "solid")+
       ggtitle("Species loss RCP4.5-SSP2")+
       guides(color = guide_legend(override.aes = list(size = 2)))+
-      xlab("")+
+      xlab(paste(guild_names[m]))+
       ylab("")+
       geom_sf(data = us_projected, fill = NA, size=0.01,color = "black")+
       geom_sf(data = canada_clipped, fill=NA,size=0.01,color = "black")+
@@ -469,6 +507,8 @@ for (i in 1:2)
       geom_sf(data = dominican_projected, fill = NA, size=0.01,color = "black")+
       coord_sf(xlim = c(-5000000 , 3000000), ylim = c(-252303 , 5980000))+
       common_theme
+    
+    
   }
   else{
     map_loss_overall[[i]]=ggplot()+
@@ -481,7 +521,7 @@ for (i in 1:2)
       #geom_sf(data=st_as_sf(north_america_cropped),size=0.1, col="black", fill=alpha("white", 0),linetype = "solid")+
       ggtitle("Species loss RCP5.8-SSP5")+
       guides(color = guide_legend(override.aes = list(size = 2)))+
-      xlab("")+
+      xlab(paste(guild_names[m]))+
       ylab("")+
       geom_sf(data = us_projected, fill = NA, size=0.01,color = "black")+
       geom_sf(data = canada_clipped, fill=NA,size=0.01,color = "black")+
@@ -504,7 +544,7 @@ for(m in 1:9){
 map_gain_overall=list()
 for (i in 1:2)
 {
-  data=get(map_data[i])[[9]]    
+  data=get(map_data[i])[[m]]    
   df5=my_function_guild_gain_overall(data)  
   df5=my_function_project(df5)
   df5$group=factor(df5$group,levels=c("0","2","3","4","1"))
@@ -521,7 +561,7 @@ for (i in 1:2)
       #geom_sf(data=st_as_sf(north_america_cropped),size=0.1, col="black", fill=alpha("white", 0),linetype = "solid")+
       ggtitle("Species gain RCP4.5-SSP2")+
       guides(color = guide_legend(override.aes = list(size = 2)))+
-      xlab("")+
+      xlab(paste(guild_names[m]))+
       ylab("")+
       geom_sf(data = us_projected, fill = NA, size=0.01,color = "black")+
       geom_sf(data = canada_clipped, fill=NA,size=0.01,color = "black")+
@@ -544,7 +584,7 @@ for (i in 1:2)
       #geom_sf(data=st_as_sf(north_america_cropped),size=0.1, col="black", fill=alpha("white", 0),linetype = "solid")+
       ggtitle("Species gain RCP5.8-SSP5")+
       guides(color = guide_legend(override.aes = list(size = 2)))+
-      xlab("")+
+      xlab(paste(guild_names[m]))+
       ylab("")+
       geom_sf(data = us_projected, fill = NA, size=0.01,color = "black")+
       geom_sf(data = canada_clipped, fill=NA,size=0.01,color = "black")+
@@ -559,9 +599,79 @@ for (i in 1:2)
 }
 different_guilds_gain[[m]]=map_gain_overall
 }
+
 setwd("/Volumes/seas-zhukai/proj-soil-fungi/land-use-climate-historical")
 
 saveRDS(data_both_effect_rcp245,file="data_both_effect_rcp245.rds")
 saveRDS(data_both_effect_rcp585,file="data_both_effect_rcp585.rds")
 
 #create the plots for different guilds
+
+plot_grid(different_guilds[[1]][[1]],different_guilds_gain[[1]][[1]],
+          different_guilds[[2]][[1]],different_guilds_gain[[2]][[1]],
+          different_guilds[[3]][[1]],different_guilds_gain[[3]][[1]],
+          different_guilds[[4]][[1]],different_guilds_gain[[4]][[1]],
+          different_guilds[[5]][[1]],different_guilds_gain[[5]][[1]],
+          different_guilds[[6]][[1]],different_guilds_gain[[6]][[1]],
+          different_guilds[[7]][[1]],different_guilds_gain[[7]][[1]],
+          different_guilds[[8]][[1]],different_guilds_gain[[8]][[1]],ncol=2)
+
+
+plot_grid(different_guilds[[1]][[2]],different_guilds_gain[[1]][[2]],
+          different_guilds[[2]][[2]],different_guilds_gain[[2]][[2]],
+          different_guilds[[3]][[2]],different_guilds_gain[[3]][[2]],
+          different_guilds[[4]][[2]],different_guilds_gain[[4]][[2]],
+          different_guilds[[5]][[2]],different_guilds_gain[[5]][[2]],
+          different_guilds[[6]][[2]],different_guilds_gain[[6]][[2]],
+          different_guilds[[7]][[2]],different_guilds_gain[[7]][[2]],
+          different_guilds[[8]][[2]],different_guilds_gain[[8]][[2]],ncol=2,labels = "auto")
+
+## to add a two variables combinations for the plot
+
+set.seed(42)
+data <- expand.grid(
+  Variable1 = factor(c( "Climate loss","Climate NA","Climate gain")),
+  Variable2 = factor(c( "Land NA", "Land loss","Land gain"))
+)
+data$Value <- runif(nrow(data))  #
+
+data$Combination <- interaction(data$Variable1, data$Variable2)
+
+# Define a custom color palette with nine distinct colors
+color_palette <- c(
+  "#E41A1C", "#377EB8", "#4DAF4A", 
+  "#984EA3", "#FF7F00", "#FFFF33", 
+  "#A65628", "#F781BF", "white"
+)
+
+data_percent%>%rename(Combination=group)%>%
+  left_join(data,by="Combination")
+
+data%>%left_join(data_percent%>%rename(Combination=group),by="Combination")->data
+
+data$Label <- c()
+
+# Plotting the grid with color mapping
+p2=ggplot(data, aes(x = Variable1, y = Variable2, fill = Combination)) +
+  geom_tile(color = "gray") +  # Add white borders for clarity
+  scale_fill_manual(values = data$color) +  # Apply the custom colors
+  labs(x = "Variable 1", y = "Variable 2", fill = "Combination") +
+  theme_minimal() +
+  theme(axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14))+
+  guides(fill="none")+
+  geom_text(aes(label = round(Freq,3)), size = 5) 
+#reorder the columns based on the effects
+
+group%>%data.frame()%>%rename_all(~paste0("Combination"))%>%left_join(data,by="Combination")
+
+
+
+#change the order of the legend
+data$Variable1=factor(data$Variable1,levels=c("Land gain","Land loss","Land NA"))
+data$Variable2=factor(data$Variable2,levels=c("Climate gain","Climate loss","Climate NA"))
+
+
+smaller_panel_grob <- ggplotGrob(p2)
+
+
