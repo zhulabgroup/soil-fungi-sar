b6e6af58a4e3df51c6172bc07ed435816efa629c
wenqisysu <wenqil@umich.edu>
2025-05-11 14:45:14 -0400

    new-folder

---
+++ peer-review-codes/MS-sar-diversity-across-scales.R
@@ -0,0 +1,555 @@
+
+library(iNEXT)
+
+setwd(".....")
+load(file="rare_all_assign.RData")
+
+###estimate fungal diversity at the 10 by 10 m scale
+
+set.seed=(1201)
+times=30
+a4=sample_data(rare_all_assign)
+a4=unique(a4$subplotID10)
+richness <- vector("list", length(a4))
+for (i in 1:length(a4))
+{
+  cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
+  data_sub <- subset_samples(rare_all_assign, subplotID10==a4[i])
+  dim1=dim(sample_data(data_sub))[1]
+  if(dim1>=2)
+  {
+    cl <- makeCluster(3)
+    registerDoParallel(cl)
+    richness[[i]] <- foreach(i = 1:times, .combine = "cbind", .packages = c("phyloseq","dplyr")) %dopar%{
+      species <- vector(length = dim1[1]) # create a vector to save diversity
+      for (j in 1:1) 
+      { 
+        # randomly sample j samples in the plot
+        flag <- rep(FALSE, dim1[1])
+        flag[sample(1:dim1[1], 1)] <- TRUE
+        temp <- merge_samples(data_sub, flag, function(x) mean(x, na.rm = TRUE)) # the j samples aggregated by mean
+        species[j] <- sum(otu_table(temp)["TRUE"] > 0)
+        return(species[j])
+      }
+    }
+    stopCluster(cl)
+  }
+  else{
+    richness[[i]]=table(colSums(otu_table(data_sub))>0)["TRUE"]%>%as.numeric()
+  }
+}
+
+# get the richness for each 10 x 10 m subplot for the NEON sites
+
+richness_subplot10_neon=data.frame(nrow=length(a4),ncol=2)# the mean indicates the mean value for the cores within the same subplot
+for (i in 1:length(a4))
+{
+  ak=dim(richness[[i]])[1]
+  
+  if(is.null(ak))
+  {
+    richness_subplot10_neon[i,1]=richness[[i]] 
+    richness_subplot10_neon[i,2]=richness[[i]] 
+  }
+  else
+  {
+    richness_subplot10_neon[i,1]=mean(richness[[i]])
+    richness_subplot10_neon[i,2]=sd(richness[[i]])
+  }
+}
+
+#####
+
+richness_subplot10_neon_standar=richness_subplot10_neon%>%mutate(plotid=substr(a4,1,8))%>%group_by(plotid)%>%summarise(mean_value=mean(nrow,na.rm=TRUE),sd_value=sd(nrow,na.rm=TRUE),area=rep(100,length.out=n))
+save(richness_subplot10_neon_standar,file="richness_subplot10_neon_standar.RData")
+
+##
+
+### at the 20 by 20 m scale
+
+set.seed=(1203)
+times=30
+a4=sample_data(rare_all_assign)
+
+a4=unique(a4$subplotID20)
+
+richness <- vector("list", length(a4))
+
+for (i in 1:length(a4))
+{
+  cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
+  data_sub <- subset_samples(rare_all_assign, subplotID20==a4[i])
+  dim1=dim(sample_data(data_sub))[1]
+  
+  if(dim1>3)
+  {
+    cl <- makeCluster(3)
+    registerDoParallel(cl)
+    richness[[i]] <- foreach(i = 1:times, .combine = "cbind", .packages = c("phyloseq","dplyr")) %dopar%{
+      species <- vector(length = dim1[1]) # create a vector to save diversity
+      for (j in 1:4) 
+      { 
+        # randomly sample j samples in the plot
+        flag <- rep(FALSE, dim1[1])
+        flag[sample(1:dim1[1], 4)] <- TRUE# select 3 cores 
+        temp <- merge_samples(data_sub, flag, function(x) mean(x, na.rm = TRUE)) # the j samples aggregated by mean
+        species[j] <- sum(otu_table(temp)["TRUE"] > 0)
+        return(species[j])
+      }
+    }
+    stopCluster(cl)
+  }
+  else if (dim1<4)
+  {
+    richness[[i]]=NA
+  }
+  else{
+    
+    richness[[i]]=table(colSums(otu_table(data_sub))>0)["TRUE"]%>%as.numeric()
+  }
+}
+
+##  get the richness for each subplot at the 20 by 20 spatial scale
+
+richness_subplot20_neon=data.frame(nrow=length(a4),ncol=2)# the mean indiactes the mean value for the cores within the same subplot
+for (i in 1:length(a4))
+{
+  
+  if(is.null(dim(richness[[i]])[1]))
+  {
+    richness_subplot20_neon[i,1]=richness[[i]] 
+    richness_subplot20_neon[i,2]=richness[[i]] 
+  }
+  
+  else
+  {
+    richness_subplot20_neon[i,1]=mean(richness[[i]])
+    richness_subplot20_neon[i,2]=sd(richness[[i]])
+  }
+}
+
+richness_subplot20_neon_standar=richness_subplot20_neon%>%mutate(plotid=substr(a4,1,8),area=rep(400,length.out=n))%>%group_by(plotid)%>%summarise(mean_value=mean(nrow,na.rm = TRUE),sd_value = sd(nrow,na.rm = TRUE))%>%mutate(area=rep(400,length.out=n))%>%rename_all(~paste0(c("plotid","mean_value","sd_value","area")))
+
+save(richness_subplot20_neon_standar,file="richness_subplot20_neon_standar.RData")
+
+
+## at the 30 by 30 m scale
+p=sample_data(rare_all_assign)%>%data.frame()%>%select(gx,gy)
+neon_data=sample_data(rare_all_assign)%>%data.frame()
+neon_data$gx=NULL
+neon_data$gy=NULL
+sample_data(rare_all_assign)=sample_data(neon_data)
+p$gx=as.numeric(p$gx)
+p$gy=as.numeric(p$gy)
+row.names(p)=row.names(sample_data(rare_all_assign))
+p=sample_data(p)
+rare_all_assign=merge_phyloseq(rare_all_assign,p)
+#####
+
+pp=sample_data(rare_all_assign)%>%data.frame()
+pp=unique(pp$plotIDM)
+ori=expand.grid(x=seq(0,10,0.5),y=seq(0,10,0.5))
+#
+
+plot_ID=sample_data(rare_all_assign)%>%data.frame()
+plot_ID=unique(plot_ID$plotIDM)
+
+set.seed(520)
+richness_plot_neon=matrix(ncol=3,nrow=length(plot_ID))
+for(k in 1:length(plot_ID))
+{
+  cat("\r", paste(paste0(rep("*", round(k / 1, 0)), collapse = ""), k, collapse = "")) # informs the processing
+  
+  core_number=numeric()
+  for(i in 1:dim(ori)[1])
+  {
+    #cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
+    
+    d=sample_data(rare_all_assign)%>%data.frame()%>%select(gx,gy,plotIDM)%>%filter(plotIDM==plot_ID[k])
+    
+    core_number[i]=subset(d,gx>=ori$x[i]&gx<=ori$x[i]+30 &gy<=ori$y[i]+30&gy>=ori$y[i])%>%summarize(row_count = n())%>%as.numeric()
+  }
+  core_number_8=core_number>8
+  core_number_8[!core_number_8]=NA
+  
+  richness=numeric()
+  for(j in 1:length(core_number))# only select the locations that over 9 cores can be sampled/81 cores
+  {
+    # cat("\r", paste(paste0(rep("*", round(j / 1, 0)), collapse = ""), j, collapse = "")) # informs the processing
+    if (j%in%c(which(!is.na(core_number_8)))){
+      # informs the processing
+      sub_samp=subset(d,gx>=ori$x[j]&gx<=ori$x[j]+30 &gy<=ori$y[j]+30&gy>=ori$y[j])
+      sub_samp=sample(rownames(sub_samp),9)# select only nine cores
+      kk=subset_samples(rare_all_assign,sample_names(rare_all_assign)%in%sub_samp)
+      richness[j]=table(colSums(otu_table(kk))>0)["TRUE"]%>%as.numeric()
+    } 
+    else{
+      sub_samp=subset(d,gx>=ori$x[j]&gx<=ori$x[j]+30 &gy<=ori$y[j]+30&gy>=ori$y[j])
+      if(dim(sub_samp)[1]<2)
+      {
+        richness[j]=NA 
+      }
+      else
+      {
+        sub_samp=subset(d,gx>=ori$x[j]&gx<=ori$x[j]+30 &gy<=ori$y[j]+30&gy>=ori$y[j])
+        sub_samp=rownames(sub_samp)# get the sample names
+        kk=subset_samples(rare_all_assign,sample_names(rare_all_assign)%in%sub_samp)#based on this to extrapolate the richness
+        data_sub =transform_sample_counts(kk, function(x) ifelse(x>0, 1, 0))
+        ot=t(otu_table(data_sub))%>%data.frame()
+        n=dim(ot)[2]# the number of "sites" for each plot
+        ot1=rowSums(ot)
+        out3 <- iNEXT(c(n,ot1), q=0, datatype="incidence_freq", size=round(seq(1, 9, length.out=9)), se=FALSE)
+        richness[j]=out3$iNextEst$size_based%>%slice_tail()%>%pull(qD)
+      }
+    }
+  } 
+  
+  richness_plot_neon[k,1]=plot_ID[k]
+  richness_plot_neon[k,2]=richness%>%mean(na.rm = TRUE)
+  richness_plot_neon[k,3]=richness%>%sd(na.rm = TRUE)
+}
+
+
+###
+
+
+#richness_subplot40_neon_standar=richness_plot_neon%>%bind_cols(a4)%>%data.frame()%>%mutate(area=rep(900,length.out=n))%>%rename_all(~paste0(c("mean_value","plotid","area")))%>%select(plotid,mean_value,area)
+
+#save(richness_subplot40_neon_standar,file="richness_subplot40_neon_standar.RData")
+
+
+## at the 40 m by 40 m scale ###
+data_40m_scale=list()
+for (i in 1:8){
+  
+}
+
+data=subset_samples(rare_all_assign,ta2==" ")
+
+set.seed=(5679)
+times=30
+a4=sample_data(rare_all_assign)
+a4=unique(a4$plotIDM)
+richness <- vector("list", length(a4))
+
+for (i in 1:length(a4))
+{
+  cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
+  data_sub <- subset_samples(rare_all_assign, plotIDM==a4[i])
+  dim1=dim(sample_data(data_sub))[1]
+  
+  if(dim1>=16)
+  {
+    cl <- makeCluster(3)
+    registerDoParallel(cl)
+    richness[[i]] <- foreach(i = 1:times, .combine = "cbind", .packages = c("phyloseq","dplyr")) %dopar%{
+      species <- vector(length = dim1[1]) # create a vector to save diversity
+      for (j in 1:16) 
+      { 
+        # randomly sample j samples in the plot
+        flag <- rep(FALSE, dim1[1])
+        flag[sample(1:dim1[1], 16)] <- TRUE
+        temp <- merge_samples(data_sub, flag, function(x) mean(x, na.rm = TRUE)) # the j samples aggregated by mean
+        species[j] <- sum(otu_table(temp)["TRUE"] > 0)
+        return(species[j])
+      }
+    }
+    stopCluster(cl)
+  }
+  else if(dim1>=2&dim1<=15)
+  {
+    sub_samp=rownames(sample_data(data_sub))
+    kk=subset_samples(rare_all_assign,sample_names(rare_all_assign)%in%sub_samp)
+    data_sub =transform_sample_counts(kk, function(x) ifelse(x>0, 1, 0))
+    ot=t(otu_table(data_sub))%>%data.frame()
+    n=dim(ot)[2]# the number of "sites" for each plot
+    ot1=rowSums(ot)
+    out3 <- iNEXT(c(n,ot1), q=0, datatype="incidence_freq", size=round(seq(1, 16, length.out=16)), se=FALSE)
+    richness[[i]]=out3$iNextEst$size_based%>%slice_tail()%>%pull(qD)
+  }
+  
+  else{
+    richness[[i]]=NA
+  }
+}
+
+
+
+richness_mean_subplot40_neon=matrix(ncol=2,nrow=length(a4))
+for (i in 1:length(a4))
+{
+  
+  richness_mean_subplot40_neon[i,1]=mean(richness[[i]])
+  richness_mean_subplot40_neon[i,2]=sd(richness[[i]])
+}
+
+
+richness_subplot40_neon_standar=cbind(plotid=a4,mean_value=richness_mean_subplot40_neon[,1],sd_value=richness_mean_subplot40_neon[,2],rep(1600,length.out=n))%>%data.frame()%>%rename_all(~paste0(c("plotid","mean_value","sd_value","area")))
+
+save(richness_subplot40_neon_standar,file="richness_subplot40_neon_standar.RData")
+#
+
+##for the dob sites
+
+##use the standardized approach to quantify the z values
+
+dob=dob_permutation
+## get the sample at the 10 by 10 scale for the dob site
+
+set.seed=(2202)
+times=30
+a4=sample_data(dob)
+
+a4=unique(a4$subplotID10)
+
+richness <- vector("list", length(a4))
+
+for (i in 1:length(a4))
+{
+  cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
+  data_sub <- subset_samples(dob, subplotID10==a4[i])
+  dim1=dim(sample_data(data_sub))[1]
+  
+  if(dim1>1)
+  {
+    cl <- makeCluster(3)
+    registerDoParallel(cl)
+    richness[[i]] <- foreach(i = 1:times, .combine = "cbind", .packages = c("phyloseq","dplyr")) %dopar%{
+      species <- vector(length = dim1[1]) # create a vector to save diversity
+      for (j in 1:1) 
+      { 
+        # randomly sample j samples in the plot
+        flag <- rep(FALSE, dim1[1])
+        flag[sample(1:dim1[1], 1)] <- TRUE# select 3 cores 
+        temp <- merge_samples(data_sub, flag, function(x) mean(x, na.rm = TRUE)) # the j samples aggregated by mean
+        species[j] <- sum(otu_table(temp)["TRUE"] > 0)
+        return(species[j])
+      }
+    }
+    stopCluster(cl)
+  }
+  else if (dim1<1)
+  {
+    richness[[i]]=NA
+  }
+  else{
+    
+    richness[[i]]=table(colSums(otu_table(data_sub))>0)["TRUE"]%>%as.numeric()
+  }
+}
+
+# get the richness at the 10 by 10
+
+richness_subplot10_dob=data.frame(nrow=length(a4),ncol=2)# the mean indiactes the mean value for the cores within the same subplot
+for (i in 1:length(a4))
+{
+  ak=dim(richness[[i]])[1]
+  
+  if(is.null(ak))
+  {
+    richness_subplot10_dob[i,1]=richness[[i]] 
+    richness_subplot10_dob[i,2]=a4[i]
+  }
+  
+  else
+  {
+    richness_subplot10_dob[i,1]=mean(richness[[i]])
+    richness_subplot10_dob[i,2]=a4[i]
+  }
+}
+
+richness_subplot10_dob%>%mutate(plotid=substr(richness_subplot10_dob$ncol,1,3))%>%dplyr::rename(richness=nrow)%>%
+  group_by(plotid)%>%summarise(mean_value=mean(richness,na.rm = TRUE),sd_value = sd(richness,na.rm = TRUE))->richness_subplot10_dob
+
+richness_subplot10_dob_standar=richness_subplot10_dob%>%mutate(area=rep(100,43))
+
+setwd("/Users/luowenqi/soil-sar/plot-sar-permutation/standardize sar")
+
+save(richness_subplot10_dob_standar,file="richness_subplot10_dob_standar.RData")
+
+#at the 20 m scale, we have already quantified this
+
+set.seed=(1203)
+times=30
+a4=sample_data(dob)
+
+a4=unique(a4$subplotID20)
+
+richness <- vector("list", length(a4))
+
+for (i in 1:length(a4))
+{
+  cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
+  data_sub <- subset_samples(dob, subplotID20==a4[i])
+  dim1=dim(sample_data(data_sub))[1]
+  
+  if(dim1>3)
+  {
+    cl <- makeCluster(3)
+    registerDoParallel(cl)
+    richness[[i]] <- foreach(i = 1:times, .combine = "cbind", .packages = c("phyloseq","dplyr")) %dopar%{
+      species <- vector(length = dim1[1]) # create a vector to save diversity
+      for (j in 1:4) 
+      { 
+        # randomly sample j samples in the plot
+        flag <- rep(FALSE, dim1[1])
+        flag[sample(1:dim1[1], 4)] <- TRUE# select 3 cores 
+        temp <- merge_samples(data_sub, flag, function(x) mean(x, na.rm = TRUE)) # the j samples aggregated by mean
+        species[j] <- sum(otu_table(temp)["TRUE"] > 0)
+        return(species[j])
+      }
+    }
+    stopCluster(cl)
+  }
+  else if (dim1<4)
+  {
+    richness[[i]]=NA
+  }
+  else{
+    
+    richness[[i]]=table(colSums(otu_table(data_sub))>0)["TRUE"]%>%as.numeric()
+  }
+}
+
+##  get the richness for each subplot at the 20 by 20 spatial scale
+
+richness_subplot20_dob=data.frame(nrow=length(a4),ncol=2)# the mean indicates the mean value for the cores within the same subplot
+for (i in 1:length(a4))
+{
+  
+  if(is.null(dim(richness[[i]])[1]))
+  {
+    richness_subplot20_dob[i,1]=richness[[i]] 
+    richness_subplot20_dob[i,2]=richness[[i]] 
+  }
+  
+  else
+  {
+    richness_subplot20_dob[i,1]=mean(richness[[i]])
+    richness_subplot20_dob[i,2]=sd(richness[[i]])
+  }
+}
+
+
+richness_subplot20_dob_standar=richness_subplot20_dob%>%mutate(plotid=substr(a4,1,3))%>%group_by(plotid)%>%summarise(mean_value=mean(nrow,na.rm = TRUE),sd_value = sd(nrow,na.rm = TRUE))%>%mutate(area=rep(400,length.out=n))
+
+save(richness_subplot20_dob_standar,file="richness_subplot20_dob_standar.RData")
+
+
+##at the 30 by 30 scale
+
+richness_plot_permu=list()
+for (p in 1:30)
+{
+  cat("\r", paste(paste0(rep("*", round(p / 1, 0)), collapse = ""), p, collapse = "")) # informs the processing
+  
+  richness_plot_dob=numeric()
+  for(k in 1:length(plot_ID))
+  {
+    
+    core_number=numeric()
+    for(i in 1:dim(ori)[1])
+    {
+      d=sample_data(dob_permutation)%>%data.frame()%>%select(X1,X2,plotIDM)%>%filter(plotIDM==plot_ID[k])
+      
+      core_number[i]=subset(d,X1>=ori$x[i]&X1<=ori$x[i]+30 &X2<=ori$y[i]+30&X2>=ori$y[i])%>%summarize(row_count = n())%>%as.numeric()
+    }
+    core_number_8=core_number>8
+    core_number_8[!core_number_8]=NA
+    richness=numeric()
+    for(j in c(which(!is.na(core_number_8))))# only select the locations that over 9 cores can be sampled/81 cores
+    {
+      # informs the processing
+      sub_samp=subset(d,X1>=ori$x[j]&X1<=ori$x[j]+30 &X2<=ori$y[j]+30&X2>=ori$y[j])
+      sub_samp=sample(rownames(sub_samp),9)# select only nine cores
+      kk=subset_samples(dob_permutation,sample_names(dob_permutation)%in%sub_samp)
+      richness[j]=table(colSums(otu_table(kk))>0)["TRUE"]%>%as.numeric()
+    } 
+    
+    richness_plot_dob[k]=richness%>%mean(na.rm = TRUE)
+  }
+  richness_plot_permu[[p]]=richness_plot_dob
+}
+
+# get the mean for the 30 permutations
+
+mean_permu=matrix(ncol=length(plot_ID),nrow=30)
+for(p in 1:30)
+{
+  mean_permu[p,] =richness_plot_permu[[p]]
+}
+
+richness_subplot30_dob_standar=mean_permu%>%data.frame()%>%t()%>%data.frame()%>%rowwise()%>% summarise(mean_value = mean(c_across(everything()), na.rm = TRUE))%>%mutate(plotID=plot_ID,area=rep(900,length.out=n))%>%select( plotID,mean_value,area)
+
+save(richness_subplot30_dob_standar,file="richness_subplot30_dob_standar.RData")
+
+
+
+#at the 40 by 40 scale
+set.seed=(5679)
+times=30
+a4=sample_data(dob)
+a4=unique(a4$plotIDM)
+richness <- vector("list", length(a4))
+
+for (i in 1:length(a4))
+{
+  cat("\r", paste(paste0(rep("*", round(i / 1, 0)), collapse = ""), i, collapse = "")) # informs the processing
+  data_sub <- subset_samples(dob, plotIDM==a4[i])
+  dim1=dim(sample_data(data_sub))[1]
+  
+  if(dim1>=16)
+  {
+    cl <- makeCluster(3)
+    registerDoParallel(cl)
+    richness[[i]] <- foreach(i = 1:times, .combine = "cbind", .packages = c("phyloseq","dplyr")) %dopar%{
+      species <- vector(length = dim1[1]) # create a vector to save diversity
+      for (j in 1:16) 
+      { 
+        # randomly sample j samples in the plot
+        flag <- rep(FALSE, dim1[1])
+        flag[sample(1:dim1[1], 16)] <- TRUE
+        temp <- merge_samples(data_sub, flag, function(x) mean(x, na.rm = TRUE)) # the j samples aggregated by mean
+        species[j] <- sum(otu_table(temp)["TRUE"] > 0)
+        return(species[j])
+      }
+    }
+    stopCluster(cl)
+  }
+  else{
+    
+    if(dim1>=2)
+    {
+      data_sub =transform_sample_counts(kk, function(x) ifelse(x>0, 1, 0))
+      ot=t(otu_table(data_sub))%>%data.frame()
+      n=dim(ot)[2]# the number of "sites" for each plot
+      ot1=rowSums(ot)
+      out3 <- iNEXT(c(n,ot1), q=0, datatype="incidence_freq", size=round(seq(1, 16, length.out=16)), se=FALSE)
+      richness[[i]]=out3$iNextEst$size_based%>%slice_tail()%>%pull(qD)
+    }
+    
+    else{
+      richness[[i]]=NA
+    }
+  }
+}
+
+
+richness_mean_subplot40_dob=matrix(ncol=1,nrow=43)
+for (i in 1:43)
+{
+  
+  richness_mean_subplot40_dob[i,]=mean(richness[[i]])
+}
+
+richness_subplot40_dob_standar=cbind(plotid=a4,richness=richness_mean_subplot40_dob,rep(1600,43))%>%data.frame()%>%rename_all(~paste0(c("plotid","richness","area")))
+
+setwd("/Users/luowenqi/soil-sar/plot-sar-permutation/standardize sar")
+
+save(richness_subplot40_dob_standar,file="richness_subplot40_dob_standar.RData")
+
+
+
+
+
+
---
+++ peer-review-codes/MS-setup-functions-packages.R
@@ -0,0 +1,155 @@
+#
+library(tigris)
+library(rnaturalearth)
+library(sf)
+library(rnaturalearth)
+library(plyr)
+library(dplyr)
+library(gstat)
+library(raster)
+library(ggplot2)
+library(car)
+library(classInt)
+library(caret)
+library(caretEnsemble)
+library(doParallel)
+library(gridExtra)
+library(terra)
+library(tidyr)
+library(geodata)
+library(reshape)
+library(SSDM)
+library(stars)
+library(patchwork)
+library(phyloseq)
+library(dplyr)
+
+#to get the sf object of the several states
+# to crop part of the canada map based on the ranges
+
+
+us_states <- states(cb = TRUE)
+canadian_provinces <- ne_states(country = "Canada", returnclass = "sf")
+cuba_provinces <- ne_states(country = "Cuba", returnclass = "sf")
+mexico_states <- ne_states(country = "Mexico", returnclass = "sf")
+rico_provinces <- ne_countries(scale = "medium", returnclass = "sf") %>%
+  dplyr::filter(admin == "Puerto Rico")
+haiti_provinces <- ne_countries(scale = "medium", returnclass = "sf") %>%
+  dplyr::filter(admin == "Puerto Rico")
+
+haiti_sf <- ne_countries(scale = "medium", country = "Haiti", returnclass = "sf")
+dominican_republic <- ne_countries(scale = "medium", returnclass = "sf", country = "Dominican Republic")
+
+baha_sf <- ne_countries(scale = "medium", country = "The Bahamas", returnclass = "sf")
+jama_sf <- ne_countries(scale = "medium", country = "Jamaica", returnclass = "sf")
+
+
+# to make projection for the sf objects
+
+target_crs <- "EPSG:5070"
+us_projected <- st_transform(us_states, crs = target_crs)
+canadian_projected <- st_transform(canadian_provinces, crs = target_crs)
+mexico_projected <- st_transform(mexico_states, crs = target_crs)
+rico_projected <- st_transform(rico_provinces, crs = target_crs)
+cuba_projected <- st_transform(cuba_provinces, crs = target_crs)
+
+haiti_projected <- st_transform(haiti_sf, crs = target_crs)
+
+dominican_projected <- st_transform(dominican_republic, crs = target_crs)
+
+baha_projected <- st_transform(baha_sf, crs = target_crs)
+jama_projected <- st_transform(jama_sf, crs = target_crs)
+
+# to clip the canada map
+bbox <- st_bbox(c(xmin = -2665004, ymin = 2157670 , xmax = 3188701, ymax = 5400000 ), crs = st_crs(canadian_projected))
+bbox_sf <- st_as_sfc(bbox)
+cropped_province <- st_crop(canadian_projected, bbox_sf)
+canada_clipped=cropped_province
+
+
+
+# the function to project the data based on a data.frame
+
+my_function_project=function(data)
+{
+  if ("x"%in%colnames(data))
+  {
+    points <- vect(data, geom = c("x", "y"), crs = "EPSG:4326")  # Assuming WGS84 coordinates
+    raster_template <- rast(ext(points), resolution = 0.17, crs = "EPSG:4326")  # Resolution of 1 degree
+    raster <- rasterize(points, raster_template, field = "value")
+  }
+  else{
+    points <- vect(data, geom = c("lon", "lat"), crs = "EPSG:4326")  # Assuming WGS84 coordinates
+    raster_template <- rast(ext(points), resolution = 0.17, crs = "EPSG:4326")  # Resolution of 1 degree
+    
+    raster <- rasterize(points, raster_template, field = "group")
+  }
+  target_crs <- "EPSG:5070"
+  raster_equal_area <- project(raster, target_crs,method="near")# there are options for the method used
+  raster_df <- as.data.frame(raster_equal_area, xy = TRUE,)
+  return(raster_df )
+}
+
+##add sf layers on the map
+
+add_sf_layers <- function() {
+  list(
+    geom_sf(data = dominican_projected, fill = NA, size = 0.01, color = "gray80"),
+    geom_sf(data = us_projected, fill = NA, size = 0.01, color = "gray80"),
+    geom_sf(data = canada_clipped, fill = NA, size = 0.01, color = "gray80"),
+    geom_sf(data = rico_projected, fill = NA, size = 0.01, color = "gray80"),
+    geom_sf(data = cuba_projected, fill = NA, size = 0.01, color = "gray80"),
+    geom_sf(data = mexico_projected, fill = NA, size = 0.01, color = "gray80"),
+    geom_sf(data = haiti_projected, fill = NA, size = 0.01, color = "gray80"),
+    geom_sf(data = baha_projected, fill = NA, size = 0.01, color = "gray80"),
+    geom_sf(data = jama_projected, fill = NA, size = 0.01, color = "gray80")
+  )
+}
+
+
+
+#set different them for the maps
+theme_map=theme(legend.spacing.y = unit(32, "pt"), 
+                legend.position = c(0.15,0.35),
+                legend.margin = margin(t = -30, r = 0, b = -1, l = 0),
+                legend.text = element_text(size=8,angle=0),
+                legend.box = "vertical",
+                legend.justification = "center",
+                legend.title = element_text(margin = margin(b = 4),size=10),
+                text = element_text(size = 18),
+                plot.title = element_text(size = 15, hjust = 0.5), 
+                axis.text.y = element_blank(), 
+                axis.text.x = element_blank(), 
+                axis.title.y = element_text(size = 18), 
+                axis.title.x = element_text(size = 18), 
+                axis.ticks.x = element_blank(), 
+                axis.ticks.y = element_blank(),
+                plot.margin = unit(c(0.3, -5, -0.5, 0.5), "cm"),
+                panel.background = element_rect(fill = "NA"),
+                panel.border = element_blank())
+
+theme_latitude=theme(legend.position = c(0.75,0.28),
+                     legend.text = element_text(size=8),
+                     legend.title  = element_text(size=10),
+                     text = element_text(size = 18),
+                     plot.title = element_text(size = 15, hjust = 0.5), 
+                     axis.text.y = element_text(size=12), 
+                     axis.text.x = element_text(size = 12), 
+                     axis.title.y = element_text(size = 15), 
+                     axis.title.x = element_text(size = 15), 
+                     plot.margin = unit(c(0.3, 0.1, -.5, 0), "cm"),
+                     panel.background = element_rect(fill = "NA"),
+                     panel.border = element_rect(color = "black", size = 0.6, fill = NA))
+
+##save the sf object
+
+sf_layers <- list(
+  dominican_projected, us_projected, canada_clipped, rico_projected,
+  cuba_projected, mexico_projected, haiti_projected, baha_projected, jama_projected
+)
+
+# Create base plot
+
+
+
+
