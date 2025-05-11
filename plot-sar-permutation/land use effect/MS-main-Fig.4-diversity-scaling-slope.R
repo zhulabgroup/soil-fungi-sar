7856b0881ee20eb57b23d7750192d60a1a01d7d3
wenqisysu <wenqil@umich.edu>
2025-05-11 14:17:37 -0400

    figure-4-in-the main-text

--- plot-sar-permutation/land use effect/MS-main-Fig.4-diversity-scaling-slope.R
+++ plot-sar-permutation/land use effect/MS-main-Fig.4-diversity-scaling-slope.R
@@ -86,33 +86,6 @@ p_zvalue=ggplot(plot_data, aes(x = area, y = mean_value, color = plotid)) +
 
 
 
-# to compare the z values among land use types
-
-load("~/soil-sar/plot-sar-permutation/model_comp.RData")
-
-model_comp%>%filter(guild=="all")%>%group_by(type)%>%dplyr::summarize(mean_value=mean(zvalue,na.rm=TRUE))
-
-model_comp%>%group_by(type)%>%summarize(mean_value=mean(zvalue,na.rm=TRUE))
-
-model_comp%>%filter(guild=="all")%>%group_by(type)%>%summarise(count = n())
-
-model_comp%>%filter(guild!="all")->guild_mean
-
-guild_mean%>%filter(!guild%in%c("para","epiphy","littersap", "woodsap" ))->guild_mean
-
-# we updated the z value analysis by excluding the parasitic and epiphytic fungi
-# based on the model to test significant difference
-
-mod=lmer(zvalue~guild+(1 | plotID),data=guild_mean)
-# to compare the means
-library(emmeans)
-emmeans_model <- emmeans(mod, ~ guild)
-pairwise_comparisons <- contrast(emmeans_model, method = "pairwise")
-summary(pairwise_comparisons, adjust = "tukey") # Adjust for multiple testing
-
-k=aggregate(zvalue~guild,data=guild_mean,FUN=mean)
-od <- k[order(k$zvalue, decreasing = TRUE),] 
-guild_mean$guild <- factor(guild_mean$guild, levels = od$guild)
 
 p_z_guild=ggplot(guild_mean,aes(x=guild,y=zvalue,fill=guild),alpha=0.5)+
   #sm_raincloud(size=0.1,point.params =list(size=2),sep_level=2)+
@@ -209,85 +182,6 @@ p11=ggplot(data=effect_no_plant%>%filter(guild=="all"),aes(x=estimate,y=1:13))+
   annotate("text",x=-0.16820583,y=3,label="*",size=8)+
   xlim(-0.5,0.5)
 
-##variance partitioning analysis
-
-## only significant variables were selected for the variance partitioning analysis
-
-sel_vab_step=list()
-for (i in 1:9)
-{
-  if(i<9)
-  {
-    mod <- lmer(zvalue ~ logc+soilInCaClpH +soilMoisture +organicCPercent +cec+ sand +bio1+ bio2 +bio4 + bio8+bio12 + bio15 + bio18  + (1 |siteIDD), data = data[[i]])
-    mod=summary(mod)
-    kk=mod$coefficients%>%data.frame()%>%filter(!Pr...t..>0.05)
-    sel_vab_step[[i]]=rownames(kk)[1:dim(kk)[1]]
-  }
-  else{
-    {
-      mod <- lmer(zvalue ~ logc+soilInCaClpH +soilMoisture +organicCPercent +cec+ sand +bio1+ bio2+bio8  + bio12 + bio15 + bio18 + (1 |siteIDD), data = data[[i]])
-      mod=summary(mod)
-      kk=mod$coefficients%>%data.frame()%>%filter(!Pr...t..>0.05)
-      sel_vab_step[[i]]=rownames(kk)[1:dim(kk)[1]]
-    }
-  }
-}
-
-
-# grouping the selected variables as climate and soil effects
-# the production of the variables were treated as a combined variable
-
-soil=c("soilInCaClpH", "nitrogenPercent", "organicCPercent","soilMoisture", "cec","sand")
-climate=c("bio1", "bio2",  "bio4", "bio8", "bio12", "bio15", "bio18")
-
-# the out put is based on the below codes
-# when soil and climate variable become constants, the model will drop these terms automatcally.
-# whether or not multiplying 10 does not affect the results
-
-var_par=list()
-var_diff=matrix(ncol=2,nrow=9)
-for (i in 1:9)
-{
-  soil_variable=data[[i]][,intersect( sel_vab_step[[i]],soil)]%>%data.frame()%>%rowwise() %>%
-    mutate(product = prod(c_across(everything())))%>%dplyr::select(product)%>%data.frame()
-  climate_variable=data[[i]][,intersect( sel_vab_step[[i]],climate)]%>%data.frame()%>%rowwise() %>%
-    mutate(product = prod(c_across(everything())))%>%dplyr::select(product)%>%data.frame()
-  #plant_variable=data[[i]][,sel_vab_step[[i]]][,which(sel_vab_step[[i]]%in%plant)]%>%data.frame()%>%rowwise() %>%
-  #mutate(product = prod(c_across(everything())))%>%select(product)%>%data.frame()
-  data_temp=bind_cols(data[[i]],soil_variable*10,climate_variable)%>%rename(soil=product...19,climate=product...20)
-  mod=lmer(zvalue~logc+soil+climate+(1|siteIDD),data=data_temp)
-  a=glmm.hp(mod,commonality=TRUE)
-  var_diff[i,]=a$r.squaredGLMM
-  var_par[[i]]=a$commonality.analysis
-}
-
-## to get the random effect part and the residuals of the model
-
-var_diff%>%data.frame()%>%mutate(ree=1-X2,randd=X2-X1)%>%rename_all(~paste0(c("fix","ran","ree","randd")))->total_var
-
-total_var%>%dplyr::select(fix,randd,ree)%>%mutate(guild=guild_select)%>%melt()->temp
-
-temp%>%dplyr::select(value,guild,variable)%>%rename_all(~paste0(c("Fractions","guild","type")))->temp
-
-# to get the fixed effect part of the model
-
-k=bind_rows(var_par[[1]]%>%data.frame(),var_par[[2]]%>%data.frame(),
-            var_par[[3]]%>%data.frame(),
-            var_par[[4]]%>%data.frame(),
-            var_par[[5]]%>%data.frame(),
-            var_par[[6]]%>%data.frame(),
-            var_par[[7]]%>%data.frame(),
-            var_par[[8]]%>%data.frame(),
-            var_par[[9]]%>%data.frame())%>%
-  mutate(guild=rep(guild_select,times=c(dim(var_par[[1]])[1],dim(var_par[[2]])[1],dim(var_par[[3]])[1],dim(var_par[[4]])[1],dim(var_par[[5]])[1],dim(var_par[[6]])[1],dim(var_par[[7]])[1],dim(var_par[[8]])[1],dim(var_par[[9]])[1])))
-
-kk=rownames(k)%>%data.frame()%>%rename_all(~paste0("type"))
-kk$type=gsub("\\d", "", kk$type)
-kk$type=gsub("\\.{3}", "", kk$type)
-var_part_data=bind_cols(k,kk)
-var_part_data$type=gsub(" ","",var_part_data$type)
-var_part_data%>%data.frame()%>%rename_all(~paste0(c("Fractions","total","guild","type")))->var_fixed# some do have the data
-var_fixed%>%dplyr::select(Fractions,guild,type)%>%filter(type!="Total")%>%bind_rows(temp)%>%filter(type!="fix")->varp_new_noplant
 
 p1=ggplot(data=varp_new_noplant%>%filter(Fractions>0&guild=="all"), aes(x = guild, y = Fractions, fill = type)) +
   geom_bar(stat = "identity",position="fill",color="black",width = 0.6)+
